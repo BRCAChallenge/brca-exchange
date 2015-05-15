10b168b105afafda12ac949e0439e811872f9df1
Max <max@soe.ucsc.edu>
2015-05-14 17:51:53 -0700

    adding BIC import code

---
+++ bic/README.txt
@@ -0,0 +1,28 @@
+# download from http://research.nhgri.nih.gov/bic/
+# username haeussle
+# password genecats
+# brca1 data
+# https://research.nhgri.nih.gov/projects/bic/Member/brca1_mutation_database.shtml
+# brca2 data 
+# https://research.nhgri.nih.gov/projects/bic/Member/brca2_mutation_database.shtml
+# datafiles are called brcaX_data.txt
+
+# convert data to vcf, CURRENTLY ONLY EXPORTS THE SNPS !!
+python convBic.py  > bicSnp.vcf
+# index
+bgzip bicSnp.vcf
+tabix -p vcf bicSnp.vcf.gz 
+
+# lift to hg38
+CrossMap.py vcf /gbdb/hg19/liftOver/hg19ToHg38.over.chain.gz bicSnp.vcf /hive/data/genomes/hg19/hg19.fa bicSnp.hg38.vcf
+#@ 2015-05-13 09:28:15: Read chain_file:
+#/gbdb/hg19/liftOver/hg19ToHg38.over.chain.gz
+#@ 2015-05-13 09:28:16: Load reference genome: /hive/data/genomes/hg19/hg19.fa
+#@ 2015-05-13 09:31:06: Total entries: 19357
+#@ 2015-05-13 09:31:06: Failed to map: 5253
+#
+# !! failed to map 5k variants!
+
+# index
+bgzip bicSnp.hg38.vcf
+tabix -p vcf bicSnp.hg38.vcf.gz 
---
+++ bic/convBic.py
@@ -0,0 +1,54 @@
+# convert from NHGRI BIC dump format to VCF
+import logging
+
+def convFile(fname, chrom, printHead, rows):
+    skipCount = 0
+    for line in open(fname):
+        fs = line.rstrip("\n").split("\t")
+        if line.startswith("Accession"):
+            # output the VCF header
+            fs = [f.replace("#", "") for f in fs]
+            fieldIds = [f.replace(" ", "") for f in fs]
+            fieldIds = [f.replace("(", "") for f in fieldIds]
+            fieldIds = [f.replace(")", "") for f in fieldIds]
+            fieldNames = fs
+            if not printHead:
+                continue
+            print "##fileformat=VCFv4.0"
+            print "##source=BIC"
+            print "##reference=hg19"
+
+            for fieldId, fieldName in zip(fieldIds, fieldNames):
+                print '##INFO=<ID=%s,Number=1,Type=String,Description="%s">' % (fieldId, fieldName)
+            print "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO"
+            continue
+
+        acc = fs[0]
+        hgvs = fs[9]
+        #ex g.41277277G>A
+        if not ">" in hgvs:
+            skipCount +=1
+            continue
+        hgvs = hgvs.replace("g.","")
+        part1, altAll = hgvs.split(">")
+        refAll = part1[-1]
+        pos = part1[:-1]
+        infos = []
+        for fieldId, field in zip(fieldIds, fs):
+            infos.append("%s=%s" % (fieldId, field.replace(" ", "_")))
+        row = [int(chrom), int(pos), acc, refAll, altAll, ".", ".", ";".join(infos)]
+        rows.append(row)
+    logging.error("skipped %d indels" % skipCount)
+
+        #acc, ex, nt, codon, baseChange, aaChange, Designation, hgvsCdna, hgvsProt, hgvsHg19, genotype, dbSnp, mutType, clinImp, category, evidence, depositor
+
+def main():
+    rows = []
+    convFile("/hive/groups/cgl/brca/phase1/bic/brca1_data.txt", "17", True, rows)
+    convFile("/hive/groups/cgl/brca/phase1/bic/brca2_data.txt", "13", False, rows)
+    rows.sort()
+    for row in rows:
+        row = [str(x) for x in row]
+        print "\t".join(row)
+
+main()
