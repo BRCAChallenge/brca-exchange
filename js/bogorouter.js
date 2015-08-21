56aaeff67e3c53d6890659954ad96ea7fc3f95b9
Brian Craft <craft@soe.ucsc.edu>
2015-08-21 14:08:56 -0700

    Cut debugging statement.

--- js/bogorouter.js
+++ js/bogorouter.js
@@ -14,7 +14,6 @@ __webpack_public_path__ = config.baseurl; // eslint-disable-line camelcase
 // https://github.com/mishoo/UglifyJS2/commit/276b9a31cda2a2ef93e7af4e966baae91a434361
 
 var path = document.location.pathname;
-console.log("path", path);
 if (path.match(/^\/$/)) {
 	require.ensure(['index'], function () {
         // XXX see above
