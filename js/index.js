5eb720ac864058edf691846fe7bdeab3da6c732e
Molly Zhang <mollyzhang@ucsc.edu>
2015-08-20 18:01:04 -0700

    refactor to completely react, add table view

--- js/index.js
+++ js/index.js
@@ -10,6 +10,8 @@ require('rx.binding');
 require('rx-dom');
 require('../custom.css');
 
+var databaseUrl = require('file!../data/brca.clinvar.vcf');
+
 var {Col, Row, Input, Modal, Button, ButtonGroup} = require('react-bootstrap');
 
 var VariantTable = require('./VariantTable');
@@ -51,30 +53,12 @@ var VCFUpload = React.createClass({
 	}
 });
 
+
 var TableView = React.createClass({
-	componentDidMount: function () {
-		Rx.DOM.click(loadbtn).subscribe(() => {
-			this.setState({dialog: true});
-		});
-	},
-	hideDialog: function () {
-		this.setState({dialog: false});
-	},
-	loadData: function (ev) {
-		// XXX add error handling
-		this.setState({data: vcf.parser()(ev.currentTarget.result)});
-	},
-	getInitialState: function () {
-		return {
-			dialog: false,
-			data: undefined
-		};
-	},
 	render: function () {
-		var {dialog, data} = this.state;
+		var {data} = this.props;
 		return (
 			<div style={{position: "relative", height: "100px"}}>
-				{dialog ? <VCFUpload dataReady={this.loadData} onRequestHide={this.hideDialog}/> : ''}
 				{data?
 					<Row>
 						<Col md={10} mdOffset={1}>
@@ -90,62 +74,173 @@ var TableView = React.createClass({
 var Title = React.createClass({
     render: function() {
         return (
-        	<div class="text-center">
-        		<h1>BRCA Challenge</h1>
-        	</div>
-        	)
+			<Row style={{marginTop: 30, marginBottom: 30}}>
+				<div className="text-center" style={{fontSize: 20, color: "#FF3399"}}>
+					<h1>BRCA Challenge</h1>
+				</div>
+			</Row>
+        )
     }
 });
 
 var NavBar = React.createClass({
     render: function() {
-    	var {onAbout, onHome, onDB, onMV} = this.props;
+    	var {activeButton} = this.props;
         return (
-            <ButtonGroup>
-                <Button onClick={onHome}>Home</Button>
-                <Button onClick={onAbout}>About</Button>
-                <Button onClick={onDB}>Database</Button>
-                <Button onClick={onMV}>My Variant</Button>
-            </ButtonGroup>
+        	<Row className="text-center">
+	            <ButtonGroup>
+	                <Button className="btn-custom" 
+	                		onClick={() => activeButton('home')}>Home</Button>
+	                <Button className="btn-custom" 
+	                		onClick={() => activeButton('about')}>About</Button>
+	                <Button className="btn-custom" 
+	                		onClick={() => activeButton('database')}>Database</Button>
+	                <Button className="btn-custom" 
+	                		onClick={() => activeButton('myVariant')}>My Variant</Button>
+	            </ButtonGroup>
+            </Row>
         )
     }
 });
 
 
-var Application = React.createClass({
+var Home = React.createClass({
+	render: function() {
+		return(
+			<div>
+				<Row style={{marginTop: 100}}>
+					<div className="text-center">place holder for home</div>
+				</Row>
+			</div>
+		)
+	}
+});
+
+var About = React.createClass({
+	render: function() {
+		return(
+			<div>
+				<Row style={{marginTop: 100}}>
+					<div className="text-center">
+						<span>place holder for about</span>
+					</div>
+				</Row>
+			</div>
+		)
+	}
+});
+
+var Database = React.createClass({
 	getInitialState: function () {
-		return {about: false, home: false, database: false, myVariant: false};
+		return {
+			data: null
+		};
 	},
-
-	onHome: function () {
-		this.setState({home: true, about:false, database: false, myVariant: false});
+	componentWillMount: function (){
+		Rx.DOM.get(databaseUrl).subscribe(data => this.setState({data: vcf.parser()(data.response)}));
 	},
-
-	onAbout: function () {
-		this.setState({about: true, home:false, database: false, myVariant: false});
-	},
-
-	onDB: function () {
-		this.setState({home: false, about:false, database: true, myVariant: false});
-	},
-
-	onMV: function () {
-		this.setState({home: false, about:false, database: false, myVariant: true});
-	},
-
 
 	render: function () {
-		var {about, home, database, myVariant} = this.state;
+		var {data} = this.state;
+		return (
+			<div>
+				<div>
+					<Row style={{marginTop: 50, marginBottom: 50}}>
+						<div className="text-center">
+							<span>place holder for database summary</span>
+						</div>
+					</Row>
+					<Row style={{marginTop: 10}}>
+						<div className="text-center">
+							<input><span className="glyphicon glyphicon-search"></span></input>
+						</div>
+					</Row>
+					<Row className="text-center" style={{fontSize: 12, color: "grey"}}>
+						search for known variants
+					</Row>
+				</div>
+
+				<div style={{position: "relative", height: "100px"}}>
+					{data?
+						<Row>
+							<Col md={10} mdOffset={1}>
+								<VariantTable data={data}/>
+							</Col>
+						</Row>
+						: ''}
+				</div>
+			</div>
+		);
+	}
+});
+
+var MyVariant = React.createClass({
+	getInitialState: function () {
+		return {
+			data: null
+		};
+	},
+
+	render: function() {
+		var {data} = this.state;
+		return(
+			<div>
+				<div className="text-center">
+					<Input ref='file' type='file' onChange={this.fileChange}/>
+				</div>
+				<div style={{position: "relative", height: "100px"}}>
+					{data?
+						<Row>
+							<Col md={10} mdOffset={1}>
+								<VariantTable data={data}/>
+							</Col>
+						</Row>
+						: ''}
+				</div>
+			</div>
+		)
+	},
+
+	dataReady: function(ev) {
+		this.setState({data: vcf.parser()(ev.currentTarget.result)});
+	},
+
+
+	fileChange: function () {
+		var file = this.refs.file.getInputDOMNode().files[0],
+		reader = new FileReader();
+		// XXX This timeout allows the UI to update (close dialog) before loading
+		// a potentially large file, which will block the UI.
+		// This might also be solved by elminating the animation on Modal close,
+		// which is probably the source of the problem.
+		window.setTimeout(() => {
+			reader.onload = this.dataReady;
+			reader.readAsText(file);
+		}, 100);
+	}
+})
+
+var Application = React.createClass({
+	getInitialState: function () {
+		return {data: null, buttonName: 'null'};
+	},
+	activeButton: function (buttonName) {
+		this.setState({buttonName: buttonName})
+	},
+	
+	hideDialog: function () {
+		this.setState({buttonName: "null"});
+	},
+	render: function () {
+		var {buttonName,data} = this.state;
 		return (
 			<div>
 				<Title />
-				<NavBar onAbout={this.onAbout} onHome={this.onHome} 
-				onDB={this.onDB} onMV={this.onMV} />
-				{about ? <div>Hello</div> : ''}
-				{home ? <div><input>variant search</input></div> : ''}
-				{database ? <div>Hello db</div> : ''}
-				{myVariant ? <div>Hello my variant</div> : ''}				
-				<TableView />
+				<NavBar activeButton={this.activeButton} />
+				{buttonName==='about' ? <About /> : ''}
+				{buttonName==='home' ? <Home /> : ''}
+				{buttonName==='database' ? <Database data={data}/> : ''}
+				{buttonName==='myVariant' ? <MyVariant dataReady={this.loadData}/> : ''}				
 			</div>
 		);
 	}
