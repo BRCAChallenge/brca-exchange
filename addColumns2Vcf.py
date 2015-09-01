5d5dec48791667ae807a1dd5a65a32775a8eb410
Molly Zhang <mollyzhang@ucsc.edu>
2015-08-31 22:17:03 -0700

    add 6 mock columns to current vcf'

--- addColumns2Vcf.py
+++ addColumns2Vcf.py
@@ -11,18 +11,33 @@ Note: this script can only handle SNPs, cann't do indels
 import hgvs.dataproviders.uta
 import hgvs.parser
 import hgvs.variantmapper
+import random
+
 
 
 def main():
-	old_vcf = open("data/brca.clinvar.10line.vcf", "r")
-	new_vcf = open("data/brca.clinvar.10line.addHGVS.vcf", "w")
+	old_vcf = open("data/brca.clinvar.10line.addHGVS.vcf", "r")
+	new_vcf = open("data/brca.clinvar.mockdata.vcf", "w")
+	new_info = ["FREQ", "BIC_P", "BIC_N", "MUTTYPE", "IARC", "DBSource"]
+
 
 	for line in old_vcf:
 		if "#" in line:
 			new_vcf.write(line)
 		else:
-			line = add_HGVS(line)
+			line = add_info(line)
 			new_vcf.write(line)
+
+def add_info(line):
+	current_info = line.strip().split("\t")[-1]
+	current_info += ";FREQ=" + str(random.random())
+	current_info += ";BIC_P=?;BIC_N=?"
+	current_info += ";MUTTYPE=" + random.choice(["missense", "nonsense", "silent"])
+	current_info += ";IARC=" + random.choice(["group 1", "group 2a", "group 2b", "group 3", "group 4"])
+	current_info += ";DBSource=" + random.choice(["Clinvar", "LOVD", "BIC", "ExAC", "1000Genomes"])
+	current_info += "\n"
+	return current_info
+
 
 def add_HGVS(line):
 	items = line.strip().split("\t")
@@ -37,7 +52,7 @@ def add_HGVS(line):
 	hgvs_g = hp.parse_hgvs_variant(hgvs_g)
 	hdp = hgvs.dataproviders.uta.connect()
 	evm = hgvs.variantmapper.EasyVariantMapper(hdp,
-        primary_assembly='GRCh37', alt_aln_method='splign')
+		primary_assembly='GRCh37', alt_aln_method='splign')
 	transcripts = evm.relevant_transcripts(hgvs_g)
 	for transcript in transcripts:
 		hgvs_c = evm.g_to_c(hgvs_g, transcript)
