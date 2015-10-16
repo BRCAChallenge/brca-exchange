176d9d70a0ff16b22643028041b3041f6962f384
Molly Zhang <mollyzhang@ucsc.edu>
2015-10-16 12:18:41 -0700

    add HGVS_cDNA for all databases using counsyl pyhgvs library

--- python_scripts/addColumns2Vcf.py
+++ python_scripts/addColumns2Vcf.py
@@ -19,10 +19,10 @@ import pyhgvs.utils as pyhgvs_utils
 
 GENOME = SequenceFileDB('../data/hg19.fa')
 
-
 def main():
     args = repeat_merging.arg_parse()
-    add_HGVS_g(args.input, args.output)
+    compare_counsyl_and_invitae("../data/merge/counsyl_hgvs_c",
+                                "../data/merge/invitae_hgvs_c")
 
 def add_HGVS_g(in_path, out_path):
     """
@@ -112,14 +112,14 @@ def add_info(line):
 
 
 def add_HGVS_c_counsyl(in_path, out_path):
-    f_in = open(in_path, "r")
-    f_out = open(out_path, "w")
-
-    ########## counsyl pyhgvs setups
+    # counsyl pyhgvs setups
     with open('../data/BRCA12.refGene.txt') as infile:
         transcripts_counsyl = pyhgvs_utils.read_transcripts(infile)
     def get_transcript(name):
         return transcripts_counsyl.get(name)
+
+    f_in = open(in_path, "r")
+    f_out = open(out_path, "w")
     line_num = 0
     unmatching_cases = 0
     for line in f_in:
@@ -133,15 +133,15 @@ def add_HGVS_c_counsyl(in_path, out_path):
         offset = int(genome_coors[1])
         ref = genome_coors[2].split(">")[0]
         alt = genome_coors[2].split(">")[1]
-        transcript_id = items[3]
+        transcript_id = items[4]
         transcript = get_transcript(transcript_id)
         hgvs_name_with_transcript = pyhgvs.variant_to_hgvs_name(
             chrom, offset, ref, alt, GENOME, transcript)
         hgvs_c = hgvs_name_with_transcript.format(use_prefix=False, use_gene=False)
 
-        if items[4] == "-":
-            items[4] = hgvs_c
-        elif items[4] == hgvs_c:
+        if items[5] == "-":
+            items[5] = hgvs_c
+        elif items[5] == hgvs_c:
             pass
         else:
             # when the enigma HGVS_cDNA is in mismatched with HGVS_cDNA converted from genome coordinate
@@ -149,16 +149,73 @@ def add_HGVS_c_counsyl(in_path, out_path):
             # print unmatching_cases
             # print items[0]
             # compare_counsyl_and_invitae(items[4], hgvs_c, genome_coors, transcript_id)
-
         new_line = "\t".join(items) + "\n"
         f_out.write(new_line)
     print "unmatching cases: ", unmatching_cases
 
-def compare_counsyl_and_invitae(enigma, hgvs_c_counsyl, genome_coors, transcript_id):
-    print "enigma: ", enigma
-    print "genome coordinate: ", genome_coors
-    print "counsyl: ", hgvs_c_counsyl
-    print "invitea: ", HGVS_c_invitae(genome_coors, transcript_id)
+def add_HGVS_c_invitae(in_path, out_path):
+    f_in = open(in_path, "r")
+    f_out = open(out_path, "w")
+    # in vitae hgvs setups
+    hp = hgvs.parser.Parser()
+    hdp = hgvs.dataproviders.uta.connect()
+    evm = hgvs.variantmapper.EasyVariantMapper(hdp,
+        primary_assembly = 'GRCh37', alt_aln_method = 'splign')
+    line_num = 0
+    unmatching_cases = 0
+    not_implemented_cases = 0
+    for line in f_in:
+        line_num += 1
+        print line_num
+        if line_num == 1:
+            f_out.write(line)
+            continue
+        items = line.strip().split("\t")
+        HGVS_genomic = items[3]
+        try:
+            hgvs_g = hp.parse_hgvs_variant(HGVS_genomic)
+            transcript_id = items[4]
+            try:
+                hgvs_c = str(evm.g_to_c(hgvs_g, transcript_id)).split(":")[1]
+            except:
+                hgvs_c = "not implemented"
+                not_implemented_cases += 1
+        except hgvs.exceptions.HGVSParseError:
+            not_implemented_cases += 1
+            hgvs_c = "not implemented"
+
+        if items[5] == "-":
+            items[5] = hgvs_c
+        elif items[5] == hgvs_c:
+            pass
+        elif hgvs_c != "not implemented":
+            unmatching_cases += 1
+        new_line = "\t".join(items) + "\n"
+        f_out.write(new_line)
+    print "unmatching cases: ", unmatching_cases
+    print "not implemented cases: ", not_implemented_cases
+
+def compare_counsyl_and_invitae(counsyl_file, invitae_file):
+    f_c = open(counsyl_file, "r")
+    f_i = open(invitae_file, "r")
+    clist = []
+    ilist = []
+    unmatching_num = 0git
+    unmatching_implemented_num = 0
+    for line in f_c:
+        clist.append(line.strip())
+    for line in f_i:
+        ilist.append(line.strip())
+    for i in range(len(clist)):
+        if clist[i] != ilist[i]:
+            unmatching_num += 1
+            if ilist[i] != "not implemented":
+                unmatching_implemented_num += 1
+                print clist[i]
+                print ilist[i]
+                print ""
+    print "unmatching cases between counsyl and invitae: ", unmatching_num
+    print "unmatching implemented cases between counsyl and invitae: ", unmatching_implemented_num
 
 
 def add_HGVS_p(in_path, out_path):
@@ -178,27 +235,6 @@ def add_HGVS_p(in_path, out_path):
     else:
         raise Exception("number of transcripts_invitae is not 1")
 
-def add_HGVS_c_invitae(genome_coors, transcript_id):
-    ########## in vitae hgvs setups
-    hp = hgvs.parser.Parser()
-    hdp = hgvs.dataproviders.uta.connect()
-    evm = hgvs.variantmapper.EasyVariantMapper(hdp,
-        primary_assembly = 'GRCh37', alt_aln_method = 'splign')
-    ### temp
-    hgvs_g = get_HGVS_g(genome_coors)
-    hgvs_g = hp.parse_hgvs_variant(hgvs_g)
-    # transcripts_invitae = evm.relevant_transcripts(hgvs_g)
-
-    # if len(transcripts_invitae) == 1:
-    #     for transcript in transcripts_invitae:
-    try:
-        hgvs_c_invitae = evm.g_to_c(hgvs_g, transcript_id)
-    except NotImplementedError:
-        return "not implemented in invitae hgvs library"
-    # else:
-    #     print transcripts_invitae
-    #     raise Exception("number of transcripts_invitae is not 1")
-    return str(hgvs_c_invitae)
 
 
 if __name__ == "__main__":
