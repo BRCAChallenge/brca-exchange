eb2cd3f0073437a256d0f30a67d73212480a128f
Brian Craft <bcboy@thecraftstudio.com>
2015-11-26 15:54:12 -0800

    Drop obsolete page.

--- js/content.js
+++ js/content.js
@@ -5,7 +5,6 @@
 var content = {
 	home: require('../content/home.md'),
 	history: require('../content/history.md'),
-	brca1_2: require('../content/brca1_2.md'), //eslint-disable-line camelcase
 	variation: require('../content/variationAndCancer.md'),
 	help: require('../content/help.md'),
     disclaimer: require('../content/disclaimer.md'),
