87be6002970a5730b7abc036c852cdb81c3f08fc
Brian Craft <bcboy@thecraftstudio.com>
2015-11-25 17:32:07 -0800

    Force json extension & drop unused actions.

--- backend/applications/brca_exchange/controllers/default.py
+++ backend/applications/brca_exchange/controllers/default.py
@@ -1,7 +1,6 @@
 def index():
-    return dict()
+    response.view = 'default/data.json'
 
-def data():
     q_source = (db.brca_variant.Variant_Source.upper().contains([request.vars.source.upper()]))
     order_by = getattr(db.brca_variant, request.vars.order_by)
     page_size = int(request.vars.page_size)
@@ -22,42 +21,3 @@ def data():
 
     brca_data = db(search_query_string & q_source).select(orderby=order_by, limitby=limit_by)
     return dict(brca_data=brca_data)
-
-def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/manage_users (requires membership in
-    http://..../[app]/default/user/bulk_register
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    """
-    return dict(form=auth())
-
-
-@cache.action()
-def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
-    return response.download(request, db)
-
-
-def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
-    return service()
-
-
