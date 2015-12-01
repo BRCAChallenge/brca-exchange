4fa6b33bbd61a5d2c8c3f5db4d000c3b6c7c660c
Brian Craft <bcboy@thecraftstudio.com>
2015-11-30 17:32:44 -0800

    Add backend.js.

---
+++ js/backend.js
@@ -0,0 +1,36 @@
+/*global require: false, module: false */
+'use strict';
+
+var Rx = require('rx');
+require('rx-dom');
+var _ = require('underscore');
+
+var databaseUrl = "http://localhost:8000";
+
+function filters(filterValues) {
+	return _.map(_.pick(filterValues, v => v != null),
+				(v, k) => `filter=${k}&filterValue=${v}`).join('&');
+}
+
+function searchColumns(columns) {
+	return _.map(columns, c => `search_column=${c}`).join('&');
+}
+
+function data(opts) {
+	var {
+		filterValues = {},
+		source = '',
+	    sortBy: {prop = 'Gene_symbol', order = 'ascending'},
+		pageLength = 100,
+		page = 1,
+		search = '',
+		searchColumn = ['Variant_Source', 'Gene_symbol']} = opts,
+		// XXX use a proper url escape API!!
+		query = `${databaseUrl}/data?${filters(filterValues)}&source=${source}&order_by=${prop}&direction=${order}&page_size=${pageLength}&page_num=${page}&search_term=${search}&${searchColumns(searchColumn)}`;
+
+	return Rx.DOM.get(query).map(xhr => JSON.parse(xhr.responseText));
+}
+
+module.exports = {
+	data
+};
