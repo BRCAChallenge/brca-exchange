494607366af143cd2414f38bdc6ca7faddfccaf7
Molly Zhang <mollyzhang@ucsc.edu>
2016-01-20 15:16:32 -0800

    removed result part

--- overlap/overlap_direct_comparison.py
+++ overlap/overlap_direct_comparison.py
@@ -1,69 +1,6 @@
 import glob
 
-
-
-"""
-    result:
-unique variants in ClinVar:6065
-overlap between ClinVar and ex_LOVD: 231
-overlap between ClinVar and LOVD: 1558
-overlap between ClinVar and 1000_Genomes: 341
-overlap between ClinVar and ExAC: 1191
-overlap between ClinVar and BIC: 3203
-overlap between ClinVar and UMD: 1395
-
-unique variants in ex_LOVD:288
-overlap between ex_LOVD and ClinVar: 231
-overlap between ex_LOVD and LOVD: 265
-overlap between ex_LOVD and 1000_Genomes: 83
-overlap between ex_LOVD and ExAC: 177
-overlap between ex_LOVD and BIC: 259
-overlap between ex_LOVD and UMD: 209
-
-unique variants in LOVD:3129
-overlap between LOVD and ClinVar: 1558
-overlap between LOVD and ex_LOVD: 265
-overlap between LOVD and 1000_Genomes: 352
-overlap between LOVD and ExAC: 911
-overlap between LOVD and BIC: 1479
-overlap between LOVD and UMD: 1168
-
-unique variants in 1000_Genomes:4351
-overlap between 1000_Genomes and ClinVar: 341
-overlap between 1000_Genomes and ex_LOVD: 83
-overlap between 1000_Genomes and LOVD: 352
-overlap between 1000_Genomes and ExAC: 520
-overlap between 1000_Genomes and BIC: 317
-overlap between 1000_Genomes and UMD: 363
-
-unique variants in ExAC:3738
-overlap between ExAC and ClinVar: 1191
-overlap between ExAC and ex_LOVD: 177
-overlap between ExAC and LOVD: 911
-overlap between ExAC and 1000_Genomes: 520
-overlap between ExAC and BIC: 924
-overlap between ExAC and UMD: 859
-
-unique variants in BIC:3633
-overlap between BIC and ClinVar: 3203
-overlap between BIC and ex_LOVD: 259
-overlap between BIC and LOVD: 1479
-overlap between BIC and 1000_Genomes: 317
-overlap between BIC and ExAC: 924
-overlap between BIC and UMD: 1178
-
-unique variants in UMD:3854
-overlap between UMD and ClinVar: 1395
-overlap between UMD and ex_LOVD: 209
-overlap between UMD and LOVD: 1168
-overlap between UMD and 1000_Genomes: 363
-overlap between UMD and ExAC: 859
-overlap between UMD and BIC: 1178
-"""
-
-
 PATH = "/hive/groups/cgl/brca/phase1/data/cutoff_vcf/"
-
 
 def main():
     databases = get_databases(PATH)
