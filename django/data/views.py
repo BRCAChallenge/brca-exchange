0c8ed5b4a58dc3c231acb68c89f168c6ca7bc2c8
Irene Papakonstantinou <irene@lshift.net>
2016-02-10 15:49:22 +0000

    Reformat tabs to spaces

--- django/data/views.py
+++ django/data/views.py
@@ -1,27 +1,25 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.core import serializers
 import json
+
+from django.core import serializers
+from django.http import JsonResponse
+
 from .models import Variant
 
 
 def index(request):
+    # data = json.load(open('/home/pete/work/BRCA/brca-website/brca.json','r'))
+    querySet = Variant.objects.all()
+    json_data = serializers.serialize('json', querySet)
 
-	#data = json.load(open('/home/pete/work/BRCA/brca-website/brca.json','r'))
-	querySet = Variant.objects.all()
-	json_data = serializers.serialize('json', querySet)
-	
-	data = json.loads(json_data)
+    data = json.loads(json_data)
 
+    header = data[0]['fields'].keys()
+    rows = [row['fields'].values() for row in data]
 
+    response_data = {'header': header, 'rows': rows}
 
-	header = data[0]['fields'].keys()
-	rows = [row['fields'].values() for row in data]
+    response = JsonResponse(response_data)
 
-	response_data = {'header' : header, 'rows' : rows}
+    response['Access-Control-Allow-Origin'] = '*'
 
-	response = JsonResponse(response_data)
-
-	response['Access-Control-Allow-Origin'] = '*'
-
-	return response
+    return response
