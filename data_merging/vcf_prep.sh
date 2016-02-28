08d1b97ff73d79aa23be838890b9241d1fb122a4
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-02-28 12:10:28 -0800

    remove vcf_prep.sh

--- data_merging/vcf_prep.sh
+++
@@ -1,3 +0,0 @@
-echo "preprocessing files..."
-cut -f1-9 ../../Data/allVcfs/1000_genomes.brca.vcf > ../../Data/allVcfs/1000_genomes.brca.no_sample.vcf
-python exac_VEP_expansion.py -i ../../Data/allVcfs/exac_BRCA12.sorted.vcf -o ../../Data/allVcfs/exac.VEP_expanded.vcf
