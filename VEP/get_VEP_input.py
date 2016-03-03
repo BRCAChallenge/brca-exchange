9cd7a5e9f205120f8cd99db3cd07bc64fa7646a9
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-03-03 15:04:50 -0800

    fix bug

--- VEP/get_VEP_input.py
+++ VEP/get_VEP_input.py
@@ -6,10 +6,14 @@ OUTPUT = "/cluster/home/mollyzhang/release1.0/data/VEP/vep_input.txt"
 
 
 def main():
-    f = tempfile.tempfile()
-    subprocess.call(["cut", "-d,", "-f3", INPUT], stdout=f)
-    for line in f:
-        print line.strip()
+    out = subprocess.check_output(["cut", "-d,", "-f3", INPUT])
+    genome_coors = out.split("\n")[1:-1]
+    f = open(OUTPUT, "w")
+    for genome_coor in genome_coors:
+        items = genome_coor[3:].replace("-", ".").split(":")
+        items = items[0:2] + ['.'] + items[2].split(">")
+        f.write(" ".join(items) + "\n")
+        
 
 if __name__ == "__main__":
     main()
