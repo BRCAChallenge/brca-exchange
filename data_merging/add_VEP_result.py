9190cb2f44702bbb62c7d1d852d075c78eed4a6d
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-03-03 15:05:06 -0800

    start script to add VEP results to merged file

---
+++ data_merging/add_VEP_result.py
@@ -0,0 +1,23 @@
+"""add VEP result to merged data"""
+from pprint import pprint
+
+
+
+MERGED_FILE = "/cluster/home/mollyzhang/release1.0/merged.csv"
+OUTPUT = "/cluster/home/mollyzhang/release1.0/merged_withVEP.csv"
+VEP = "/cluster/home/mollyzhang/release1.0/data/VEP/vep_output_3_3_2016.vcf"
+
+
+def main():
+    save_VEP_to_dict()
+
+
+def save_VEP_to_dict():
+    f
+
+
+
+
+if __name__ == "__main__":
+    main()
+
