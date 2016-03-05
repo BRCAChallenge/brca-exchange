cfe99552d918202b55155e2032605d5fde34c963
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-03-05 15:47:12 -0800

    finish script

--- data_merging/add_VEP_result.py
+++ data_merging/add_VEP_result.py
@@ -1,9 +1,7 @@
 """add VEP result to merged data"""
-from pprint import pprint
-import vcf
 import pandas as pd
 import json
-
+import pickle
 
 MERGED_FILE = "/cluster/home/mollyzhang/release1.0/merged.csv"
 OUTPUT = "/cluster/home/mollyzhang/release1.0/merged_withVEP.csv"
@@ -21,16 +19,46 @@ VEP_FIELDS = ['Allele', 'Consequence', 'IMPACT', 'SYMBOL', 'Gene',
               'PUBMED', 'MOTIF_NAME', 'MOTIF_POS', 'HIGH_INF_POS',
               'MOTIF_SCORE_CHANGE']
 
+
+
 def main():
-    save_VEP_to_dict().values()
+    vep_result_dict = save_VEP_to_dict()
+    temp_dump = open("temp_dump", "w")
+    temp_dump.write(pickle.dumps(vep_result_dict))
+    temp_dump.close()
+    f_in = open(MERGED_FILE, "r")
+    f_out = open(OUTPUT, "w")
+    line_num = 0
+    for line in f_in:
+        line_num += 1
+        items = line.strip().split(",")
+        if line_num == 1:
+            vep_fields = ["VEP" + i for i in VEP_FIELDS]
+            items += vep_fields
+        else:
+            genome_coor = items[2]
+            if genome_coor in vep_result_dict.keys():
+                additional_items = []
+                for column in VEP_FIELDS:
+                    additional_items.append(vep_result_dict[genome_coor][column])
+            items += additional_items
+        new_line = ",".join(items) + "\n"
+        f_out.write(new_line)
+
+
 
 def save_VEP_to_dict():
+    print("processing VEP output")
     f = open(VEP_OUTPUT, "r")
     vep_dict = {}
+    line_no = 0
     for line in f:
         if "#" in line:
             continue
 
+        line_no += 1
+        if line_no%10 == 0: 
+            print(line_no)
         items = line.replace(".", "-").strip().split("\t")
         genome_coor = "chr{0}:{1}:{2}>{3}".format(items[0],
                                                   items[1],
@@ -40,7 +68,7 @@ def save_VEP_to_dict():
             if items[-1] != "-":
                 vep_dict[genome_coor] = process_VEP_info(items[-1])
         else:
-            raise Exception("bad time") 
+            raise Exception("bad time")
     return vep_dict
 
 def process_VEP_info(info):
@@ -51,11 +79,17 @@ def process_VEP_info(info):
         result_dict = dict(zip(VEP_FIELDS, values))
         row_list.append(result_dict)
     df = pd.DataFrame(row_list)
-    collapsed = []
+    collapsed = {}
     for field in df.columns:
-        for key, value in df[field].value_counts().iteritems():
-            collapsed.append(key, "{0}/{1}".format(value, len(infos)))
-    return collapsed 
+        this_field = df[field].value_counts()
+        if len(this_field) > 1:
+            value_list = []
+            for key, value in df[field].value_counts().iteritems():
+                value_list.append([key, "{0}/{1}".format(value, str(len(infos)))])
+            collapsed[field] = json.dumps(value_list)
+        else:
+            collapsed[field] = this_field.index[0]
+    return collapsed
 
 
 if __name__ == "__main__":
