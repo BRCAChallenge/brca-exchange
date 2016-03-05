2f12e4f169e8ba7fd07f1368bf7b1caa6489774a
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-03-05 12:56:34 -0800

    add listerized git fields

--- data_merging/add_VEP_result.py
+++ data_merging/add_VEP_result.py
@@ -5,14 +5,25 @@ import pandas as pd
 
 MERGED_FILE = "/cluster/home/mollyzhang/release1.0/merged.csv"
 OUTPUT = "/cluster/home/mollyzhang/release1.0/merged_withVEP.csv"
-VEP = "/cluster/home/mollyzhang/release1.0/data/VEP/vep_output_3_3_2016.vcf"
-VEP_FIELDS = "Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|DISTANCE|STRAND|SYMBOL_SOURCE|HGNC_ID|TSL|APPRIS|REFSEQ_MATCH|SIFT|PolyPhen|GMAF|AFR_MAF|AMR_MAF|EAS_MAF|EUR_MAF|SAS_MAF|AA_MAF|EA_MAF|ExAC_MAF|ExAC_Adj_MAF|ExAC_AFR_MAF|ExAC_AMR_MAF|ExAC_EAS_MAF|ExAC_FIN_MAF|ExAC_NFE_MAF|ExAC_OTH_MAF|ExAC_SAS_MAF|CLIN_SIG|SOMATIC|PHENO|PUBMED|MOTIF_NAME|MOTIF_POS|HIGH_INF_POS|MOTIF_SCORE_CHANGE"
+VEP_OUTPUT = "/cluster/home/mollyzhang/release1.0/data/VEP/vep_output_3_3_2016.vcf"
+VEP_FIELDS = ['Allele', 'Consequence', 'IMPACT', 'SYMBOL', 'Gene',
+              'Feature_type', 'Feature', 'BIOTYPE', 'EXON', 'INTRON',
+              'HGVSc', 'HGVSp', 'cDNA_position', 'CDS_position',
+              'Protein_position', 'Amino_acids', 'Codons',
+              'Existing_variation', 'DISTANCE', 'STRAND', 'SYMBOL_SOURCE',
+              'HGNC_ID', 'TSL', 'APPRIS', 'REFSEQ_MATCH', 'SIFT', 'PolyPhen',
+              'GMAF', 'AFR_MAF', 'AMR_MAF', 'EAS_MAF', 'EUR_MAF', 'SAS_MAF',
+              'AA_MAF', 'EA_MAF', 'ExAC_MAF', 'ExAC_Adj_MAF', 'ExAC_AFR_MAF',
+              'ExAC_AMR_MAF', 'ExAC_EAS_MAF', 'ExAC_FIN_MAF', 'ExAC_NFE_MAF',
+              'ExAC_OTH_MAF', 'ExAC_SAS_MAF', 'CLIN_SIG', 'SOMATIC', 'PHENO',
+              'PUBMED', 'MOTIF_NAME', 'MOTIF_POS', 'HIGH_INF_POS',
+              'MOTIF_SCORE_CHANGE']
 
 def main():
     save_VEP_to_dict().values()
 
 def save_VEP_to_dict():
-    f = open(VEP, "r")
+    f = open(VEP_OUTPUT, "r")
     vep_dict = {}
     for line in f:
         if "#" in line:
@@ -31,20 +42,19 @@ def save_VEP_to_dict():
     return vep_dict
 
 def process_VEP_info(info):
-    unwanted_fields = "Allele|SYMBOL|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|DISTANCE|STRAND|SYMBOL_SOURCE|HGNC_ID|ExAC_MAF|ExAC_Adj_MAF|ExAC_AFR_MAF|ExAC_AMR_MAF|ExAC_EAS_MAF|ExAC_FIN_MAF|ExAC_NFE_MAF|ExAC_OTH_MAF|ExAC_SAS_MAF".split("|")
-
     infos = info[4:].split(",")
     row_list = []
     for each_info in infos:
-        columns = VEP_FIELDS.split("|")
         values = each_info.split("|")
-        result_dict = dict(zip(columns, values))
-        for field in unwanted_fields:
-            
-        row_list. 
-
+        result_dict = dict(zip(VEP_FIELDS, values))
+        row_list.append(result_dict)
     df = pd.DataFrame(row_list)
-    print df.head()
+    for field in df.columns:
+        print field
+        print df[field]
+        print df[field].value_counts()
+        print type(df[field].value_counts())    
+        print "--------------------------------"
     return df 
 
 
