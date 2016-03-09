fd00cb6c5239fd9f556e0e65af52cd44451b50b2
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-03-08 18:57:13 -0800

    clean up vep results

--- data_merging/add_VEP_result.py
+++ data_merging/add_VEP_result.py
@@ -4,7 +4,7 @@ import json
 import pickle
 
 MERGED_FILE = "/cluster/home/mollyzhang/release1.0/merged.csv"
-OUTPUT = "/cluster/home/mollyzhang/release1.0/merged_withVEP.csv"
+OUTPUT = "/cluster/home/mollyzhang/release1.0/merged_withVEP_cleaned.csv"
 VEP_OUTPUT = "/cluster/home/mollyzhang/release1.0/data/VEP/vep_output_3_3_2016.vcf"
 VEP_FIELDS = ['Allele', 'Consequence', 'IMPACT', 'SYMBOL', 'Gene',
               'Feature_type', 'Feature', 'BIOTYPE', 'EXON', 'INTRON',
@@ -18,28 +18,36 @@ VEP_FIELDS = ['Allele', 'Consequence', 'IMPACT', 'SYMBOL', 'Gene',
               'ExAC_OTH_MAF', 'ExAC_SAS_MAF', 'CLIN_SIG', 'SOMATIC', 'PHENO',
               'PUBMED', 'MOTIF_NAME', 'MOTIF_POS', 'HIGH_INF_POS',
               'MOTIF_SCORE_CHANGE']
-
+UNWANTED = ["Allele", "SYMBOL", "Gene", "Feature_type", "Feature", "HGVSc",
+            "HGVSp", "cDNA_position", "CDS_position", "Protein_position",
+            "SYMBOL_SOURCE", "HGNC_ID", "GMAF", "AFR_MAF", "AMR_MAF", "EAS_MAF",
+            "EUR_MAF", "SAS_MAF", "AA_MAF", "EA_MAF", "ExAC_MAF", 'ExAC_Adj_MAF',
+            'ExAC_AFR_MAF', 'ExAC_AMR_MAF', 'ExAC_EAS_MAF', 'ExAC_FIN_MAF',
+            'ExAC_NFE_MAF', 'ExAC_OTH_MAF', 'ExAC_SAS_MAF', "CLIN_SIG"]
 
 
 def main():
     vep_result_dict = save_VEP_to_dict()
-    #temp_dump = open("temp_dump", "w")
-    #temp_dump.write(pickle.dumps(vep_result_dict))
-    #temp_dump.close()
+    temp_dump = open("temp_dump", "w")
+    temp_dump.write(pickle.dumps(vep_result_dict))
+    temp_dump.close()
     #vep_result_dict = pickle.loads(open("temp_dump", "r").read())
     write_to_file(vep_result_dict)
 
 def write_to_file(vep_result_dict):
+    print "write to file {0}".format(OUTPUT)
     f_in = open(MERGED_FILE, "r")
     f_out = open(OUTPUT, "w")
     line_num = 0
+    for field in UNWANTED:
+        VEP_FIELDS.remove(field)
     for line in f_in:
         line_num += 1
-        if line_num %100 == 0:
+        if line_num %1000 == 0:
             print line_num
         items = line.strip().split(",")
         if line_num == 1:
-            vep_fields = ["VEP_" + i for i in VEP_FIELDS]
+            vep_fields = [i+"(VEP)" for i in VEP_FIELDS]
             items += vep_fields
         else:
             genome_coor = items[2]
@@ -62,7 +70,6 @@ def save_VEP_to_dict():
     for line in f:
         if "#" in line:
             continue
-
         line_no += 1
         if line_no%10 == 0: 
             print(line_no)
@@ -73,12 +80,13 @@ def save_VEP_to_dict():
                                                   items[4])
         if genome_coor not in vep_dict:
             if items[-1] != "-":
-                vep_dict[genome_coor] = process_VEP_info(items[-1])
+                vep_dict[genome_coor] = majority_vote_VEP(items[-1])
         else:
             raise Exception("bad time")
     return vep_dict
 
-def process_VEP_info(info):
+def collapse_VEP_info(info):
+    """collapse various VEP results into one by giving each one a weight"""
     infos = info[4:].split(",")
     row_list = []
     for each_info in infos:
@@ -97,6 +105,23 @@ def process_VEP_info(info):
         else:
             collapsed[field] = this_field.index[0]
     return collapsed
+
+def majority_vote_VEP(info):
+    """take the majority vote VEP"""
+    infos = info[4:].split(",")
+    row_list = []
+    for each_info in infos:
+        values = each_info.split("|")
+        result_dict = dict(zip(VEP_FIELDS, values))
+        for key in UNWANTED:
+            del result_dict[key]
+        row_list.append(result_dict)
+    df = pd.DataFrame(row_list)
+    majority = {}
+    for field in df.columns:
+        majority[field] = df[field].value_counts().index[0]
+    return majority
+
 
 
 if __name__ == "__main__":
