492f7e1c773d0f8ff454c72b1a44a5d075089112
Pete Rolph <pete@lshift.net>
2016-03-15 13:45:48 +0000

    change column names in standard filters

--- js/VariantTable.js
+++ js/VariantTable.js
@@ -43,9 +43,9 @@ function renderCell(val) {
 }
 
 var filterColumns = [
-    {name: 'Gene', prop: 'Gene_symbol', values: ['BRCA1', 'BRCA2']},
+    {name: 'Gene', prop: 'Gene_Symbol', values: ['BRCA1', 'BRCA2']},
 //    {name: 'Exon', values: ['Any', 1, 2, 3, 4, 5]}, // XXX needs refgene to get exon count
-    {name: 'Pathogenicity', prop: 'Clinical_significance', values: ['Pathogenic', 'Benign']}
+    {name: 'Pathogenicity', prop: 'Clinical_significance_ENIGMA', values: ['Pathogenic', 'Benign']}
 ];
 
 // XXX duplicate this functionality on the server, perhaps
