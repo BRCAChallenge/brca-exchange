9b5d875188c7a7fdc0a181fa84c09541597f8b6a
Pete Rolph <pete@lshift.net>
2016-04-01 12:14:21 +0100

    move captcha secret into settings.py

--- django/brca/settings.py
+++ django/brca/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&wy54&8++2d+%57ipbcuw#utl0f@!9sd6j*r1-cy@ihgz1%eae'
+CAPTCHA_SECRET = '6LdwNBwTAAAAABi4FQqI_W8qzPiuDypc4re1DjuI'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- django/users/config.py
+++
@@ -1,1 +0,0 @@
-captcha_secret = "6LdwNBwTAAAAABi4FQqI_W8qzPiuDypc4re1DjuI"
