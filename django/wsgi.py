a33855c9e99e135159d4a7e9770a30844a04b1cf
Pete Rolph <pete@lshift.net>
2016-04-01 19:33:52 +0100

    fix wsgi.py

--- django/wsgi.py
+++ django/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+appdir = '/var/www/backend/beta/django'
+sys.path.append(appdir)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brca.settings")
 
