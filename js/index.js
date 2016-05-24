9968635c0a197ec37e5e7be8c3a13a7387836b64
Charles Markello <cmarkell@ucsc.edu>
2016-05-24 14:02:57 -0700

    Fixed home link on the footer of the website to correctly link to the homepage.

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ var Footer = React.createClass({
             <div className="container footer">
                 <div className="col-sm-5 left-footer">
                     <ul>
-                        <li><a href="/home">Home</a></li>
+                        <li><a href="/">Home</a></li>
                         <li><a href="/about/history">About</a></li>
                         <li><a href="/variants">Variants</a></li>
                         <li><a href="/help">Help</a></li>
