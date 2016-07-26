29e862495d6d1d144495caed34a49c94496161e1
Joe Thomas <joathoma@ucsc.edu>
2016-07-26 13:13:00 -0700

    Give user option to resend validation link. #133

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
-    captcha_key: '6LdwNBwTAAAAACFRvttQc08debhGzAzNY0xWQhxw',
+    captcha_key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+    //backend_url: 'http://brcaexchange.cloudapp.net/backend'
     backend_url: 'http://localhost:8000'
 };
