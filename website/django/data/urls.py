8173208b4efed1e72cb469e3ef9c464522df4d65
Abraham Chavez <achave11@ucsc.edu>
2016-08-18 14:53:02 -0700

    The 5 endpoint require the URL pointers which are included in this file

--- website/django/data/urls.py
+++ website/django/data/urls.py
@@ -3,6 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
     url(r'^$', views.index, name="index"),
-    url(r'^suggestions/$', views.autocomplete)
+    url(r'^suggestions/$', views.autocomplete),
+    url(r'^ga4gh/variants/search$', views.variant_search, name='variant_search'),
+    url(r'^ga4gh/variants/(?P<variant_id>.+)$', views.get_var_by_id, name = 'get_var_by_id'),
+    url(r'^ga4gh/variantsets/search', views.get_variantSet, name='get_variantSet'),
+    url(r'^ga4gh/variantsets/(?P<variantSetId>.+)$', views.get_varset_by_id, name='get_varset_by_id'),
+    url(r'^ga4gh/datasets/search', views.search_datasets, name='search_datasets'),
+    url(r'^ga4gh/variantsets', views.varsetId_empty_catcher, name='empty_request_catcher'),
+    url(r'^ga4gh/variants', views.empty_varId_catcher, name='empty_varId_catcher')
+
 ]
