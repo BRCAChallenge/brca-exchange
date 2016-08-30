aa0ceba736e105ba65e0e7a245868dd4c6d3929b
Joe Thomas <joathoma@ucsc.edu>
2016-08-30 12:12:54 -0700

    Fix password update in profile #68

--- website/django/users/views.py
+++ website/django/users/views.py
@@ -58,8 +58,11 @@ def update(request):
     fields['has_image'] = (user[0].has_image or fields['has_image']) and not delete_image
 
     del fields['email']
-    if fields['password'] == '':
-        del fields['password']
+    if fields['password'] != '':
+        u = user[0]
+        u.set_password(fields['password'])
+        u.save()
+    del fields['password']
 
     # mailing list toggle
     if 'subscribe' in request.POST:
