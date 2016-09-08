4754164aa79fbd580f33e9c941eb72464c6cdc18
Joe Thomas <joathoma@ucsc.edu>
2016-09-08 15:08:17 -0700

    Merge admin cleanup, add manage.py command to email admins about unapproved users.

--- website/django/users/management/commands/emailreports.py
+++ website/django/users/management/commands/emailreports.py
@@ -27,17 +27,18 @@ class Command(BaseCommand):
         else:
             self.stdout.write("There are %i users awaiting approval." % user_count)
 
+            recipients = MyUser.objects.filter(admin_notifications = True).filter(is_admin = True).values_list('email', flat=True)
             plaintext_email = get_template(os.path.join(settings.BASE_DIR, 'users', 'templates', 'users_awaiting_approval_email.txt'))
             html_email = get_template(os.path.join(settings.BASE_DIR, 'users', 'templates', 'users_awaiting_approval_email.html'))
             # get backend url somehow?
             url = "http://localhost:8000/admin/users/unapproveduser/"
             d = Context({'user_count': user_count, 'url': url})
 
-            subject, from_email, to = 'admin notification test', 'noreply@brcaexchange.org', 'joathoma@ucsc.edu'
+            subject, from_email = 'admin notification test', 'noreply@brcaexchange.org'
             text_content = plaintext_email.render(d)
             html_content = html_email.render(d)
-            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg = EmailMultiAlternatives(subject, text_content, from_email, recipients)
             msg.attach_alternative(html_content, "text/html")
             msg.send()
 
-            self.stdout.write("email sent")
+            self.stdout.write(self.style.SUCCESS("email sent"))
