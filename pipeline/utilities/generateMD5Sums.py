964d3f3ad86dcb70d3b5e70ca9cd3abe791ce6e3
Zack Fischmann <zacharyrobert@gmail.com>
2016-10-14 12:17:41 -0500

    prevent hashing the hash output file

--- pipeline/utilities/generateMD5Sums.py
+++ pipeline/utilities/generateMD5Sums.py
@@ -12,18 +12,17 @@ def main():
     args = parser.parse_args()
 
     output_file_name = args.outputFile
-
     # Recurses through a directory and it's subdirectories and generates md5 hashes for each file
     # All hashes are appended to an output file as specified with the -o flag.
     with open(output_file_name, 'w') as f_out:
-    for subdir, dirs, files in os.walk(args.inputDir):
-        for file in files:
-            # Don't hash the output file
-            if file == output_file_name:
-                continue
-            filename = os.path.join(subdir, file)
-            md5hash = hashlib.md5(open(filename, 'rb').read()).hexdigest()
-            f_out.write(file + ": " + md5hash + '\n')
+        for subdir, dirs, files in os.walk(args.inputDir):
+            for file in files:
+                # Don't hash the output file
+                if file == output_file_name.split('/')[-1]:
+                    continue
+                filename = os.path.join(subdir, file)
+                md5hash = hashlib.md5(open(filename, 'rb').read()).hexdigest()
+                f_out.write(file + ": " + md5hash + '\n')
 
 if __name__ == "__main__":
     main()
