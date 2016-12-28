7594dad96314b871c90a01534d249c2541c41d2c
Zack Fischmann <zacharyrobert@gmail.com>
2016-12-28 14:43:59 -0800

    update test cases

--- pipeline/utilities/test_releaseDiff.py
+++ pipeline/utilities/test_releaseDiff.py
@@ -1,6 +1,6 @@
 import pytest
 import unittest
-from releaseDiff import equivalentSemicolonDelimitedValues
+from releaseDiff import equivalentPathogenicityAllValues
 
 
 class TestStringMethods(unittest.TestCase):
@@ -8,22 +8,22 @@ class TestStringMethods(unittest.TestCase):
     def test_reordered_pathogenicity_all_data(self):
         prev = "Uncertain_significance,Likely_benign (ClinVar); Pending (BIC)"
         new = "Likely_benign,Uncertain_significance (ClinVar); Pending (BIC)"
-        self.assertTrue(equivalentSemicolonDelimitedValues(prev, new))
+        self.assertTrue(equivalentPathogenicityAllValues(prev, new))
 
     def test_swapped_pathogenicity_all_data(self):
         prev = "Uncertain_significance,Likely_benign (ClinVar); Pending (BIC)"
         new = "Uncertain_significance,Likely_benign (BIC); Pending (ClinVar)"
-        self.assertFalse(equivalentSemicolonDelimitedValues(prev, new))
+        self.assertFalse(equivalentPathogenicityAllValues(prev, new))
 
     def test_different_pathogenicity_all_data(self):
         prev = "Uncertain_significance,Likely_benign (ClinVar); Pending (BIC)"
         new = "Likely_benign (ClinVar); Pending (BIC)"
-        self.assertFalse(equivalentSemicolonDelimitedValues(prev, new))
+        self.assertFalse(equivalentPathogenicityAllValues(prev, new))
 
     def test_same_pathogenicity_all_data_single_source(self):
         prev = "Uncertain_significance,Likely_benign (ClinVar)"
         new = "Likely_benign,Uncertain_significance (ClinVar)"
-        self.assertTrue(equivalentSemicolonDelimitedValues(prev, new))
+        self.assertTrue(equivalentPathogenicityAllValues(prev, new))
 
 
 if __name__ == '__main__':
