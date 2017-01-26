9b37cdcb7a8f03f939736ba6fb73197ffeaa1739
Joe Thomas <joathoma@ucsc.edu>
2017-01-26 15:15:24 -0800

    Remove commented import

--- website/django/data/management/commands/add_diff_json.py
+++ website/django/data/management/commands/add_diff_json.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand, CommandError
-# from django.conf import settings
 from django.db import connection
 from data.models import Variant, VariantDiff 
 from argparse import FileType
