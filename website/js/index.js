f98dedaffd16ccfb64f280488fd71a8a6a663601
Zack Fischmann <zacharyrobert@gmail.com>
2017-02-17 14:30:49 -0800

    use vimeo video, make responsive

--- website/js/index.js
+++ website/js/index.js
@@ -146,10 +146,7 @@ var Home = React.createClass({
                 </Row>
                 <Row>
                     <Col md={8} mdOffset={2} className="text-center video-container">
-                        <iframe src="https://player.vimeo.com/video/199396428" width="640" height="360" frameBorder="0" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe>
-                        <div className="wistia_responsive_padding video-container" style={videoStyles}><div className="wistia_responsive_wrapper" style={videoWrapperStyles}><iframe src="//fast.wistia.net/embed/iframe/dl2zrjowtv?videoFoam=true" title="Wistia video player" allowTransparency="true" frameBorder="0" scrolling="no" className="wistia_embed" name="wistia_embed" allowFullScreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="100%" height="100%"></iframe></div></div>
-                        <script src="//fast.wistia.net/assets/external/E-v1.js" async></script>
-                        <iframe style={youtubeStyles} width="560" height="315" src="https://www.youtube.com/embed/9avj2pYYN38" frameBorder="0" allowFullScreen></iframe>
+                        <iframe src="https://player.vimeo.com/video/199396428" className="vimeo-video" frameBorder="0" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe>
                     </Col>
                 </Row>
                 <Row className="logo-block">
