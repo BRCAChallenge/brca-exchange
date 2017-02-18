b364418bb27c93af2c3ff990646e79dbec04c35e
Zack Fischmann <zacharyrobert@gmail.com>
2017-02-17 16:07:46 -0800

    catch newly created lists of data as new instead of considering them additions to an existing list

--- website/js/index.js
+++ website/js/index.js
@@ -395,16 +395,16 @@ function getDisplayName(key) {
 
 // test for the various forms of blank fields
 function isEmptyField(value) {
-    if (value === null) {
-        return true;
-    }
-
     if (Array.isArray(value)) {
         value = value[0];
     }
 
+    if (value === null || (typeof value === 'undefined')) {
+        return true;
+    }
+
     var v = value.trim();
-    return v === '' || v === '-' || v === 'None' || v === undefined;
+    return v === '' || v === '-' || v === 'None';
 }
 
 function isEmptyDiff(value) {
@@ -490,7 +490,15 @@ var VariantDetail = React.createClass({
                     }
 
                     if (added !== null || removed !== null) {
-                        if (fieldDiff.field_type === "list") {
+                        if (isEmptyField(removed)) {
+                            diffHTML.push(
+                                <span>
+                                    <strong>{ getDisplayName(fieldName) }: </strong>
+                                    <span className='label label-success'><span className='glyphicon glyphicon-star'></span> New</span>
+                                    &nbsp;{added}
+                                </span>, <br />
+                            );
+                        } else if (fieldDiff.field_type === "list") {
                             diffHTML.push(
                                 <span>
                                     <strong>{ getDisplayName(fieldName) }: </strong> <br />
@@ -498,22 +506,12 @@ var VariantDetail = React.createClass({
                                 </span>, <br />
                             );
                         } else if (fieldDiff.field_type === "individual") {
-                            if (isEmptyField(removed)) {
-                                diffHTML.push(
-                                    <span>
-                                        <strong>{ getDisplayName(fieldName) }: </strong>
-                                        <span className='label label-success'><span className='glyphicon glyphicon-star'></span> New</span>
-                                        &nbsp;{added}
-                                    </span>, <br />
-                                );
-                            } else {
-                                diffHTML.push(
-                                    <span>
-                                        <strong>{ getDisplayName(fieldName) }: </strong>
-                                        {removed} <span className="glyphicon glyphicon-arrow-right"></span> {added}
-                                    </span>, <br />
-                                );
-                            }
+                            diffHTML.push(
+                                <span>
+                                    <strong>{ getDisplayName(fieldName) }: </strong>
+                                    {removed} <span className="glyphicon glyphicon-arrow-right"></span> {added}
+                                </span>, <br />
+                            );
                         }
                     }
                 }
