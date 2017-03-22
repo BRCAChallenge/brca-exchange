4bdcf945ef27a0bb0af99e00e70dc221e658b7d0
Zack Fischmann <zacharyrobert@gmail.com>
2017-03-22 12:35:58 -0700

    make check for change types optional in variantsBySource script

--- pipeline/utilities/variantsBySource.py
+++ pipeline/utilities/variantsBySource.py
@@ -7,8 +7,10 @@ import collections
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", default="built.tsv",
+    parser.add_argument("-i", "--input", default="built.tsv",
                         help="File with data to check")
+    parser.add_argument("-c", "--change_type", default=False,
+                        help="Report on change types if True")
 
     num_variants_by_source = {
                               "ClinVar": 0,
@@ -38,12 +40,14 @@ def main():
         for source in num_variants_by_source.keys():
             if source in variant["Source"]:
                 num_variants_by_source[source] += 1
-        for change_type in num_variants_by_change_type.keys():
-            if change_type in variant["change_type"]:
-                num_variants_by_change_type[change_type] += 1
+        if args.change_type:
+          for change_type in num_variants_by_change_type.keys():
+              if change_type in variant["change_type"]:
+                  num_variants_by_change_type[change_type] += 1
 
     print num_variants_by_source
-    print num_variants_by_change_type
+    if args.change_type:
+        print num_variants_by_change_type
     print "Total number of variants: %s" % (total_number_of_variants)
 
 if __name__ == "__main__":
