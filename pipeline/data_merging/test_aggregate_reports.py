bb6ada300b7c7d7b719401e1a9fd316f50e8ca87
Zack Fischmann <zacharyrobert@gmail.com>
2017-04-12 18:37:50 +0200

    remove pdb

--- pipeline/data_merging/test_aggregate_reports.py
+++ pipeline/data_merging/test_aggregate_reports.py
@@ -4,7 +4,6 @@ import tempfile
 import csv
 from os import path, getcwd
 import aggregate_reports
-import pdb
 
 
 VCF_TESTDATA_FILENAME = path.join(path.dirname(__file__), 'test_files/1000_Genomes.vcf')
