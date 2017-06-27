9fc4086c3c65f4b1cf354b0b1d17567c6995a42c
Zack Fischmann <zacharyrobert@gmail.com>
2017-06-26 18:03:41 -0700

    improve search tests, cause to fail for collin to fix

--- website/django/data/searchtest.py
+++ website/django/data/searchtest.py
@@ -14,13 +14,28 @@ from data import test_data
 from data.views import index, autocomplete
 
 
+'''
+NOTE:
+We use materialized views of variants to handle user queries more efficiently.
+Because of this, we need to test both the Variant and CurrentVariant models.
+The CurrentVariant model is a materialized view of the Variant model.
+see https://www.postgresql.org/docs/9.3/static/rules-materializedviews.html for
+more information about materialized views. The models can be found in data/models.py.
+'''
+
+
+'''
+TODO:
+Fix the broken tests in this file. To fix the tests, DO NOT CHANGE the assertions, instead
+change the code prior to the assertions so the assertions are truthy.
+'''
+
+
 def create_variant_and_materialized_view(variant_data):
-    '''
-    We use materialized views of variants to handle user queries more efficiently.
-    Because of this, we need to test both the Variant and CurrentVariant models.
-    Note that the CurrentVariant model is a materialized view of the Variant model.
-    https://www.postgresql.org/docs/9.3/static/rules-materializedviews.html
-    '''
+    """
+    This is a convenience method that creates/saves a new Variant and refreshes the materialized view
+    at the same time (meaning a new CurrentVariant is created to match the Variant).
+    """
     variant = Variant.objects.create_variant(row=variant_data)
     with connection.cursor() as cursor:
         cursor.execute("REFRESH MATERIALIZED VIEW currentvariant")
@@ -30,11 +45,16 @@ def create_variant_and_materialized_view(variant_data):
 
 class VariantTestCase(TestCase):
     def setUp(self):
+        """
+        All tests have access to anything created in this method. Right now, all we need is to set up a request factory
+        and create a variant and a materialized view of that same variant. The variant data is pulled from the
+        test_data.py file in this same directory.
+        """
         self.factory = RequestFactory()
         (self.existing_variant, self.existing_variant_materialized_view) = create_variant_and_materialized_view(test_data.existing_variant())
 
     def test_variant_model(self):
-        """Create a new variant and then retrieve it by the Genomic_Coordinate_hg38 column"""
+        """This tests creation and retreival of a new variant by the Genomic_Coordinate_hg38 column"""
         Variant.objects.create_variant(row=(test_data.new_variant()))
         new_variant_genomic_coordinate_hg38 = test_data.new_variant()['Genomic_Coordinate_hg38']
         retrieved_variant = Variant.objects.get(Genomic_Coordinate_hg38='chr17:g.43070959:A>G')
@@ -42,10 +62,7 @@ class VariantTestCase(TestCase):
         self.assertEqual(retrieved_variant.Genomic_Coordinate_hg38, new_variant_genomic_coordinate_hg38)
 
     def test_current_variant_model(self):
-        """
-        Create a new variant and then retrieve it by the Genomic_Coordinate_hg38 column
-        Then ensure that the CurrentVariant and Variant have the same data
-        """
+        """This tests creation of a new Variant and that the associated CurrentVariant has the same basic properties."""
         (new_variant, new_current_variant) = create_variant_and_materialized_view(test_data.new_variant())
         new_current_variant_genomic_coordinate_hg38 = test_data.new_variant()['Genomic_Coordinate_hg38']
         retrieved_variant = Variant.objects.get(Genomic_Coordinate_hg38=new_current_variant_genomic_coordinate_hg38)
@@ -55,13 +72,12 @@ class VariantTestCase(TestCase):
 
     def test_index_resource_json(self):
         """
-        Searching for all the data in json format returns a JsonResponse
-        NOTE: There is an error in this test. Can you find the error and fix the test?
+        Tests search for all data in json format returns a JsonResponse
         Hint: how does the index method in views.py handle Variant models vs. CurrentVariant models?
         Double hint: what type of model is self.existing_variant?
         """
         request = self.factory.get(
-            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=&include=Variant_in_ENIGMA')
+            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=&include=Variant_in_ENIGMA&include=Variant_in_ClinVar&include=Variant_in_1000_Genomes&include=Variant_in_ExAC&include=Variant_in_LOVD&include=Variant_in_BIC&include=Variant_in_ESP&include=Variant_in_exLOVD')
         request.variant = self.existing_variant_materialized_view
 
         # This calls the index method from data/views.py where user queries are processed.
@@ -72,19 +88,19 @@ class VariantTestCase(TestCase):
         self.assertEqual(json.loads(response.content)['count'], 1)
 
     def test_index_resource_csv(self):
-        """Searching for all the data in csv format returns an HttpResponse"""
+        """Tests searching for all the data in csv format returns an HttpResponse"""
         request = self.factory.get(
-            'format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=&include=Variant_in_ENIGMA&include=Variant_in_ClinVar&include=Variant_in_1000_Genomes&include=Variant_in_ExAC&include=Variant_in_LOVD&include=Variant_in_BIC&include=Variant_in_ESP&include=Variant_in_exLOVD')
+            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=&include=Variant_in_ENIGMA&include=Variant_in_ClinVar&include=Variant_in_1000_Genomes&include=Variant_in_ExAC&include=Variant_in_LOVD&include=Variant_in_BIC&include=Variant_in_ESP&include=Variant_in_exLOVD')
         response = index(request)
 
         self.assertIsInstance(response, HttpResponse)
         self.assertEqual(response.status_code, 200)
 
     def test_search_by_id(self):
-        """Searching for a variant by id using a filter should return the expected result"""
-        existing_id = self.existing_variant_materialized_view.id
+        """Tests searching for a variant by id using a filter"""
+        existing_current_variant_id = self.existing_variant_materialized_view.id
         request = self.factory.get(
-            '/data/?format=json&filter=id&filterValue=%s&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&include=Variant_in_ENIGMA&search_term=' % existing_id)
+            '/data/?format=json&filter=id&filterValue=%s&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=&include=Variant_in_ENIGMA&include=Variant_in_ClinVar&include=Variant_in_1000_Genomes&include=Variant_in_ExAC&include=Variant_in_LOVD&include=Variant_in_BIC&include=Variant_in_ESP&include=Variant_in_exLOVD' % 5)
         response = index(request)
 
         self.assertIsInstance(response, JsonResponse)
@@ -97,22 +113,34 @@ class VariantTestCase(TestCase):
         self.assertEqual(response_variant["Genomic_Coordinate_hg38"], self.existing_variant.Genomic_Coordinate_hg38)
 
     def test_source_filters_all_off(self):
-        """Searching for a variant by id with variant source filtered out should return no results"""
-        existing_variant_source = 'BIC'
+        """Tests all source filters on returns no variants"""
         request = self.factory.get(
-            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&include=Variant_in_%s&search_term=' % existing_variant_source)
+            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=')
         response = index(request)
 
-        # self.assertIsInstance(response, JsonResponse)
-        # self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(response.status_code, 200)
+
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data["count"], 0)
+
+    def test_source_filter_for_variant_excludes_variant(self):
+        """Tests all source filters on returns no variants"""
+        request = self.factory.get(
+            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=&include=Variant_in_ENIGMA')
+        response = index(request)
+
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(response.status_code, 200)
 
         response_data = json.loads(response.content)
         self.assertEqual(response_data["count"], 0)
 
     def test_search_by_genomic_coordinate(self):
+        '''Tests that searching for a variant with a genomic_coordinate_hg38 search term is successful'''
         existing_genomic_coordinate = self.existing_variant_materialized_view.Genomic_Coordinate_hg38
         request = self.factory.get(
-            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&include=Variant_in_ENIGMA&search_term=%s' % existing_genomic_coordinate)
+            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&include=Variant_in_ENIGMA&search_term=%s' % "chr17:999999:A>G")
         response = index(request)
 
         self.assertIsInstance(response, JsonResponse)
@@ -125,9 +153,10 @@ class VariantTestCase(TestCase):
         self.assertEqual(response_variant["Genomic_Coordinate_hg38"], self.existing_variant.Genomic_Coordinate_hg38)
 
     def test_search_by_combined_gene_symbol_and_genomic_coordinate(self):
-        gene_symbol_genomic_coordinate_hg38 = self.existing_variant_materialized_view.Gene_Symbol + ':' + self.existing_variant_materialized_view.Genomic_Coordinate_hg38
+        """Tests that searching for a variant with a 'gene_symbol:genomic_coordinate_hg38' search term is successful"""
+        gene_symbol_genomic_coordinate_hg38 = self.existing_variant_materialized_view.Gene_Symbol + '_' + self.existing_variant_materialized_view.Genomic_Coordinate_hg38
         request = self.factory.get(
-            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&include=Variant_in_ENIGMA&search_term=%s' % gene_symbol_genomic_coordinate_hg38)
+            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=%s&include=Variant_in_ENIGMA&include=Variant_in_ClinVar&include=Variant_in_1000_Genomes&include=Variant_in_ExAC&include=Variant_in_LOVD&include=Variant_in_BIC&include=Variant_in_ESP&include=Variant_in_exLOVD' % gene_symbol_genomic_coordinate_hg38)
         response = index(request)
 
         self.assertIsInstance(response, JsonResponse)
@@ -139,19 +168,20 @@ class VariantTestCase(TestCase):
         response_variant = response_data["data"][0]
         self.assertEqual(response_variant["Genomic_Coordinate_hg38"], self.existing_variant.Genomic_Coordinate_hg38)
 
-    # @skip("Not yet implemented")
+    @skip("Functionality not yet implemented")
     def test_search_by_combined_gene_symbol_and_genomic_coordinate_with_space(self):
+        """Tests searching for 'gene_symbol genomic_coordinate_hg38' is successful"""
         """
-        NOTE: This is an example of a test for future functionality. It fails because we have not implemented code
+        NOTE: This is an example of a test for future functionality. It fails (as it should) because we have not implemented code
         to support this search type (only colon delimiters at the moment, not spaces). Failing tests are incredibly useful
         to developers when they implement a new feature that would cause the test to pass. Writing tests to define the
         parameters of a new feature prior to implementation is known as Test Driven Development (TDD).
 
-        If you find the failure bothersome, uncomment the skip line above the test. 
+        If you find the failure bothersome, uncomment the skip line above the test.
         """
         gene_symbol_genomic_coordinate_hg38 = self.existing_variant_materialized_view.Gene_Symbol + ' ' + self.existing_variant_materialized_view.Genomic_Coordinate_hg38
         request = self.factory.get(
-            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&include=Variant_in_ENIGMA&search_term=%s' % gene_symbol_genomic_coordinate_hg38)
+            '/data/?format=json&order_by=Gene_Symbol&direction=ascending&page_size=20&page_num=0&search_term=%s&include=Variant_in_ENIGMA&include=Variant_in_ClinVar&include=Variant_in_1000_Genomes&include=Variant_in_ExAC&include=Variant_in_LOVD&include=Variant_in_BIC&include=Variant_in_ESP&include=Variant_in_exLOVD' % gene_symbol_genomic_coordinate_hg38)
         response = index(request)
 
         self.assertIsInstance(response, JsonResponse)
@@ -164,7 +194,9 @@ class VariantTestCase(TestCase):
         self.assertEqual(response_variant["Genomic_Coordinate_hg38"], self.existing_variant.Genomic_Coordinate_hg38)
 
     '''
-    TODO: Write tests for each of the following searches, as well as some tests to confirm that
+    TODO:
+
+    Add tests for each of the following searches, as well as some tests to confirm that
     faulty search strings return no results.
 
     User submitted search --> Field:Field
