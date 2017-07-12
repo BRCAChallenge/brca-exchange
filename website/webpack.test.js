62474ccd373b8b104ee486726bc1db96f8568292
Faisal <falquaddoomi@ucla.edu>
2017-07-12 11:37:06 +0200

    Providing circleCI a resolved path to the build dir to see if that'll allow it to run the tests.

--- website/webpack.test.js
+++ website/webpack.test.js
@@ -1,7 +1,9 @@
-var webpack = require('webpack');
-var config = require('./webpack.config');
+/*global __dirname: false */
+const webpack = require('webpack');
+const path = require('path');
+const config = require('./webpack.config');
 
 config.output.filename = "testBundle.js";
-config.output.path = "./build";
+config.output.path = path.resolve(__dirname, "build");
 config.entry = './test/all.js';
 module.exports = config;
