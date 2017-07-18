7dfc70158af4545249d3d5eca83ea09f107ec859
meredith705 <memeredith@csumb.edu>
2017-07-18 10:00:22 -0700

    Update BRCA_Mutalyer.py

--- BRCA_Mutalyer.py
+++ BRCA_Mutalyer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 # Melissa Meredith (meredith705)
 
-# Runs Mutalyzer and returns binary flags for intronic and 'out of range' variants
+# Runs Mutalyzer and returns a binary flags for intronic and 'out of range' variants
 # In the event of truncation it returns the locaiton of a stop codon
 
 import sys
@@ -30,31 +30,29 @@ def mutalyzer(self, file, HGVS_list):
 	        if (m.errorcode == "ENOINTRON"):
 	        	intronic = 1
 	        	inTranscript = 0
+	        	NewstopCodon = 0
 	        elif (m.errorcode == "ERANGE"):
 	        	inTranscript = 0
 	        	intronic = 0
-
+	        	NewstopCodon = 0
+	elif mm.newProtein:
 	# if no error messages then variant is in transcript
-	else:
-		if mm != "messages":
-			intronic = 0
-			inTranscript = 1
+		intronic = 0
+		inTranscript = 1
 
-	if mm == "newProtein":
-
-		if len(mm.newProtein)< len(mm.origProtein) and len(mm.newCDS) < len(mm.origCDS) and len(mm.mutatedMRNA) < len(mm.origMRNA): 
+		#if mm.newProtein == "mm.newProtein":
+		if len(mm.newProtein) < len(mm.origProtein) and len(mm.newCDS) < len(mm.origCDS) and len(mm.mutatedMRNA) < len(mm.origMRNA): 
 		    orig_stop = mm.origProtein.index('*')
 		    new_stop = mm.newProtein.index('*')
 		    if orig_stop != new_stop:
-		    	NewstopCodon = len(new_stop-1)
-		        print "NewstopCodon:", NewstopCodon
-	else:
-		NewstopCodon = 0
+		    	NewstopCodon = new_stop
+		else:
+			NewstopCodon = 0
 
 	
 	
 	file.write("%d\t%d\t%d\n" % (intronic, inTranscript, NewstopCodon))
-	#print intronic, "\t", inTranscript, "\t",  NewstopCodon
+	print intronic, "\t", inTranscript, "\t",  NewstopCodon
 	return(intronic, inTranscript, NewstopCodon)
 	
 
@@ -62,7 +60,7 @@ def mutalyzer(self, file, HGVS_list):
 HGVS_list = ['NM_007294.3:c.5467+8G>A', 'NM_007294.3:c.2783G>A', 'NM_007294.3:c.1931G>T', 'NM_007294.3:c.441+5A>G', 'NM_007294.3:c.-2943G>A', 'NM_007294.3:c.134+2161G>A', 'NM_007294.3:c.1508delA']
 
 #print "intron\tinTscrip\tNewStop" 
-file = open('workfile', 'w')
+file = open('Mutalyzerfile', 'w')
 file.write("intronic\tinTranscript\tNewstopCodon\n")
 
 for variant in HGVS_list:
