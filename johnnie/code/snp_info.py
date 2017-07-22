c1ee5d577646629e1d51875674197ea34b370946
Johnnie Chang <cchan60@ucsc.edu>
2017-07-21 17:50:03 -0700

    data parsing and exprs

---
+++ johnnie/code/snp_info.py
@@ -0,0 +1,98 @@
+import vcf
+import math
+from collections import Counter
+from pprint import pprint
+import numpy as np
+import pickle
+from matplotlib import pyplot as plt
+import utilities
+import config
+import os
+
+data_path = config.data_path
+
+def shannon_entropy(reader):
+  """ Computes the Shannon Entropy of each SNP.
+      Returns a list of entropies.
+  """
+  entropies = []
+  count = 0
+  for snp in reader:
+    if count > 10:
+      #break
+      pass
+    R = len(snp.alleles)
+    probs = utilities.allele_frequencies(snp)
+    for i in range(R):
+      if probs[i] <= 0:
+        print snp
+    entropy = -1.0 * sum([probs[i] * math.log(probs[i]) if probs[i] > 0 else 0 for i in range(R)])
+    entropies.append((count, entropy))
+    count += 1
+  return entropies
+
+def simpson_index(reader):
+  simpson_indices = []
+  count = 0
+  for snp in reader:
+    if count > 10:
+      #break
+      pass
+    R = len(snp.alleles)
+    probs = utilities.allele_frequencies(snp)
+    simpson_index = sum([probs[i]**2 for i in range(R)])
+    simpson_indices.append((count, simpson_index))
+    count += 1
+  return simpson_indices
+
+def shannon_and_simpson(use_toy):
+  input_fn = '1000G_brca.hg38.vcf'
+  toy_fn = input_fn[:-4]+'.toy.vcf' if use_toy else input_fn
+  if not os.path.exists(data_path+toy_fn):
+    utilities.make_toy(input_fn, toy_fn, 20)
+
+  vcf_reader = vcf.Reader(open(data_path+toy_fn, 'rb'))
+  shannon = sorted(shannon_entropy(vcf_reader), key=lambda i : i[1], reverse=True)
+  vcf_reader = vcf.Reader(open(data_path+toy_fn, 'rb'))
+  simpson = sorted(simpson_index(vcf_reader), key=lambda i : i[1], reverse=False)
+  pprint(zip(shannon, simpson))
+  l1 = [shannon[i][0] for i in range(len(shannon))]
+  l2 = [simpson[i][0] for i in range(len(simpson))]
+  #print l1
+  #print l2
+  #print zip(l1, l2)
+  #pprint(shannon)
+  #pprint(simpson)
+  # => Entropy and Simpson give similar ranking; expected as both based on probs
+
+def uniqueness(input_fn, use_dob, dobs=None):
+  """ Computes uniqueness of individuals in the dataset. """
+  vcf_reader = vcf.Reader(open(data_path+input_fn, 'rb'))
+  m = len(vcf_reader.samples)
+  n = sum(1 for _ in vcf_reader)
+  vcf_reader = vcf.Reader(open(data_path+input_fn, 'rb'))
+  matrix1 = np.matrix(np.zeros(shape=(n, m)), dtype=str)
+  matrix2 = np.matrix(np.zeros(shape=(n, m)), dtype=str)
+
+  for i, snp in enumerate(vcf_reader):
+    for j, ind in enumerate(snp.samples):
+      matrix1[i, j], matrix2[i, j] = ind.gt_bases.split('|')
+  matrix1 = matrix1.transpose()
+  matrix2 = matrix2.transpose()
+
+  seqs = []
+  for i in range(m):
+    snp_str1 = ''.join(matrix1[i].tolist()[0])
+    snp_str2 = ''.join(matrix2[i].tolist()[0])
+    if use_dob:
+      seqs.append(snp_str1 + snp_str2 + str(dobs[i]))
+    else:
+      seqs.append(snp_str1 + snp_str2)
+  counter = Counter(seqs)
+
+  o = '' if use_dob else 'o'
+  print 'num of unique SNP strings merged, w/' + o + ' DOB', len(counter)
+  #print 'num of groups of persons sharing, w/' + o + ' DOB', len([v for v in counter.values() if v > 1])
+
+  return 1.0*len(counter)/m
+
---
+++ johnnie/code/utilities.py
@@ -0,0 +1,168 @@
+import vcf
+import random
+from collections import Counter
+import random
+import datetime
+from datetime import date, timedelta
+import calendar
+import pickle
+import numpy as np
+import config
+import os
+
+data_path = config.data_path
+
+def exon_starts_ends(input_fn, names):
+  exon_starts = []
+  exon_ends = []
+  with open(data_path+input_fn, 'rb') as infile:
+    for line in infile:
+      if line.startswith('#'):
+        continue
+      fields = line.split('\t')
+      name = fields[1]
+      if name in names:
+        exon_starts += fields[9].split(',')
+        exon_ends   += fields[10].split(',')
+  assert len(exon_starts) == len(exon_ends), 'exon starts and ends differ in number'
+  return exon_starts, exon_ends
+
+exon_starts, exon_ends = exon_starts_ends('RefSeq.genepred', ['NM_000059.3', 'NM_007294.3'])
+
+def extract_exon(input_fn, output_fn):
+  """ Extract SNPs on exons and write to new file. """
+  if os.path.exists(data_path+output_fn):
+    print "Regenerating", data_path+output_fn
+
+  def on_exon(line):
+    pos = line.split('\t')[1]
+    for i in range(len(exon_starts)):
+      start, end = exon_starts[i], exon_ends[i]
+      if (pos > start) and (pos < end):
+        return True
+    return False
+
+  with open(data_path+output_fn, 'wb') as outfile:
+    with open(data_path+input_fn, 'rb') as infile:
+      for line in infile:
+        if (line.startswith('#')) or (on_exon(line)):
+          outfile.write(line)
+
+def dump_records(input_fn):
+  """ Dump a VCF file as list of vcf.model._Record. """
+  vcf_reader = vcf.Reader(open(data_path+input_fn, 'rb'))
+  records = [record for record in vcf_reader]
+  pickle.dump(records, open(data_path+input_fn+'.pickle', 'wb'))
+
+def make_toy(input_fn, output_fn, p):
+  """ Sample lines with p% probability. """
+  if os.path.exists(data_path+output_fn):
+    print "Regenerating", data_path+output_fn
+
+  snp_num = 0
+  snp_sam = 0
+  with open(data_path+output_fn, 'wb') as outfile:
+    with open(data_path+input_fn, 'rb') as infile:
+      for line in infile:
+        if line.startswith('#'):
+          outfile.write(line)
+        else:
+          snp_num += 1
+          if random.random() < 0.01*p:
+            outfile.write(line)
+            snp_sam += 1
+
+def allele_frequencies(record):
+  """ Allele frequencies of record; [0] is frequency of REF allele. """
+  allele_counts = Counter()
+  num_chroms = 0.0
+  for s in record.samples:
+    if s.gt_type is not None:
+      for a in s.gt_alleles:
+        allele_counts.update([a])
+        num_chroms += 1
+  return [allele_counts[str(i)]/num_chroms for i in range(0, len(record.ALT)+1)]
+
+def synthetic_headers(line, p):
+  """ Generate tab separated headers of synthetic samples. """
+  line = line.split('FORMAT')[1]
+  sample_num = len(line.split('\t'))
+  synth_num = int(sample_num * p * 0.01)
+  synth = ['SY'+str(i).zfill(5) for i in range(synth_num)]
+  choices = random.sample(xrange(sample_num), synth_num)
+  return sample_num, '\t'.join(synth), choices
+
+def synthetic_records(line, person_choices, synthetic):
+  """ Generate tab separated SNP values of synthetic coreferent individuals. """
+  if person_choices == None:
+    return
+  fields = line.split('\t')
+  alleles = set([fields[3]] + fields[4].split(','))
+  sample = line.split('\t')[9:]
+  values = [sample[c] for c in person_choices]
+  if synthetic: # randomly pick one from possible alleles?
+    values = [random.choice(list(alleles - set([values[c]]))) for c in person_choices]
+  return '\t'.join(values)
+
+def make_synthetic_coreference(input_fn, output_fn, p):
+  """ Generate VCF file with synthetic coreferent records,
+      using p% of data in input_fn. """
+  if os.path.exists(data_path+output_fn):
+    print "Regenerating", data_path+output_fn
+
+  snp_num = 0
+  with open(data_path+input_fn, 'rb') as infile:
+    for line in infile:
+      if not line.startswith('#'):
+        snp_num += 1
+  snp_choices = random.sample(xrange(snp_num), int(snp_num * p * 0.01))
+  print snp_choices
+  person_choices = None
+  snp_count = 0
+  with open(data_path+output_fn, 'wb') as outfile:
+    with open(data_path+input_fn, 'rb') as infile:
+      for line in infile:
+        if line.startswith('##'): # meta information
+          outfile.write(line)
+        elif line.startswith('#'): # header
+          sample_num, syn_headers, person_choices = synthetic_headers(line, 10)
+          outfile.write(line.replace('\n', '\t'))
+          outfile.write(syn_headers + '\n')
+        else:
+          outfile.write(line.replace('\n', '\t'))
+          outfile.write(synthetic_records(line, person_choices, snp_count in snp_choices) + '\n')
+        snp_count += 1
+  return sample_num
+
+def all_possible_dobs(min_age, max_age):
+  """ Return all possible dates of birth of each year, given a range of ages. """
+  now = datetime.datetime.now()
+  min_year = now.year - max_age
+  max_year = now.year - min_age
+  dobs = []
+  for year in range(min_year, max_year+1):
+    frst_day = date(year, 1, 1)
+    last_day = date(year, 12, 31)
+    delta = last_day - frst_day
+    dobs += [frst_day + timedelta(days=i) for i in range(delta.days + 1)]
+    #if calendar.isleap(year):
+    #  years.append([i for i in range(366)])
+    #else:
+    #  years.append([i for i in range(365)])
+  return dobs
+
+def synthetic_dob(m):
+  """ Sample dates of birth of m persons. """
+  dobs = all_possible_dobs(20, 65)
+  return np.random.choice(dobs, m, replace=True)
+
+def fill_zeros(m, max_len):
+  """ Append zeros to each list in m until they have length max_len;
+      return a matrix as an np.array.
+  """
+  for l in m:
+    if len(l) < max_len:
+      for i in range(max_len - len(l)):
+        l.append(0)
+  return np.array([l for l in m])
+
