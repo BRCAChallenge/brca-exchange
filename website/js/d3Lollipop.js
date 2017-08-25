d5f4c6b1468226efecba139ac05b3785927a734a
Charles Markello <cmarkell@ucsc.edu>
2017-08-24 17:14:35 -0700

    fixup! Adding in allele frequency information into plot

--- website/js/d3Lollipop.js
+++ website/js/d3Lollipop.js
@@ -8,15 +8,21 @@ require('./css/d3Lollipop.css');
 var Mutneedles = require("muts-needle-plot");
 var PureRenderMixin = require('./PureRenderMixin');
 
-var {Grid, Row, Nav, NavItem} = require('react-bootstrap');
+var {Grid, Row, Nav, DropdownButton, MenuItem} = require('react-bootstrap');
 
 var Spinjs = require('spin.js');
 
 var brca12JSON = {
-    BRCA1: {
+    BRCA1Path: {
         brcaDomainFile: require('raw!../content/brca1LollipopDomain.json')
     },
-    BRCA2: {
+    BRCA1Allele: {
+        brcaDomainFile: require('raw!../content/brca1LollipopDomain.json')
+    },
+    BRCA2Path: {
+        brcaDomainFile: require('raw!../content/brca2LollipopDomain.json')
+    },
+    BRCA2Allele: {
         brcaDomainFile: require('raw!../content/brca2LollipopDomain.json')
     }
 };
@@ -24,27 +30,39 @@ var brca12JSON = {
 
 var d3Lollipop = {};
 
-d3Lollipop.drawStuffWithD3 = function(ref, muts, domain, id, varlink) {
+d3Lollipop.drawStuffWithD3 = function(ref, muts, domain, brcakey, varlink) {
 
     var xAxisLabel = '';
     var minPos = 0;
     var maxPos = 1;
-    if (id === 'BRCA1') {
+    if (brcakey.indexOf('BRCA1') !== -1) {
         xAxisLabel = 'Coordinate Selection (GRCh38 chr 17)';
         minPos = 43000000;
         maxPos = 43180000;
-    } else if (id === 'BRCA2') {
+    } else if (brcakey.indexOf('BRCA2') !== -1) {
         xAxisLabel = 'Coordinate Selection (GRCh38 chr 13)';
         minPos = 32300000;
         maxPos = 32410000;
     }
     var legends = {x: xAxisLabel, y: ""};
     var colorMap = {
-      // mutation categories
+      // Mutation pathogenicity categories
       "Pathogenic": "red",
       "Benign": "lightblue"
     };
-    var config = {variantDetailLink: varlink, minCoord: minPos, maxCoord: maxPos, mutationData: muts, regionData: domain, targetElement: ref.id, legends: legends, colorMap: colorMap };
+    if (brcakey.indexOf('Allele') !== -1) {
+        colorMap = {
+            // Allele frequency categories
+            "Uncertain": "purple",
+            "<0.0001": "red",
+            "<0.001": "pink",
+            "<0.01": "lightblue",
+            "<0.05": "blue",
+            "<0.5": "lightgreen",
+            ">=0.5": "green"
+        };
+    };
+    var config = {variantDetailLink: varlink, minCoord: minPos, maxCoord: maxPos, mutationData: muts, regionData: domain, targetElement: ref.id, legends: legends, colorMap: colorMap, brcakey: brcakey};
     var instance =  new Mutneedles(config);
     return function() {
         instance.tip.destroy();
@@ -110,8 +128,24 @@ var D3Lollipop = React.createClass({
         }
         // Process Allele Frequency format
         var maxAlleleFreq = parseFloat(oldObj["Max_Allele_Frequency"]);
-        var newObj = {category: oldObj.Pathogenicity_expert, coord: chrCoordinate, alleleFreq: maxAlleleFreq, value: 1, oldData: obj};
-        console.log('filtered data :', newObj);
+        var maxAlleleFreqCategory = "";
+        if (maxAlleleFreq < 0.0001) {
+            maxAlleleFreqCategory = "<0.0001";
+        } else if (maxAlleleFreq < 0.001) {
+            maxAlleleFreqCategory = "<0.001";
+        } else if (maxAlleleFreq < 0.01) {
+            maxAlleleFreqCategory = "<0.01";
+        } else if (maxAlleleFreq < 0.05) {
+            maxAlleleFreqCategory = "<0.05";
+        } else if (maxAlleleFreq < 0.5) {
+            maxAlleleFreqCategory = "<0.5";
+        } else if (maxAlleleFreq >= 0.5) {
+            maxAlleleFreqCategory = ">=0.5";
+        } else {
+            maxAlleleFreqCategory = "Uncertain";
+        };
+        var newObj = {category: oldObj.Pathogenicity_expert, coord: chrCoordinate, alleleFreqCategory: maxAlleleFreqCategory, alleleFreq: maxAlleleFreq, value: 1, oldData: obj};
+        //console.log('filtered data :', newObj);
         return newObj;
     },
     componentDidMount: function() {
@@ -119,6 +153,7 @@ var D3Lollipop = React.createClass({
         var {data, brcakey, onRowClick, ...opts} = this.props;
         var d3svgBrcaRef = React.findDOMNode(this.refs.d3svgBrca);
         var subSetData = data.map(this.filterAttributes);
+        console.log('BRCAKEY: ', brcakey);
         var domainBRCA = JSON.parse(brca12JSON[brcakey].brcaDomainFile);
         // Don't render chart if there's no data recieved yet
         if (this.props.data.length !== 0) {
@@ -157,7 +192,7 @@ var Lollipop = React.createClass({
     mixins: [PureRenderMixin],
     getInitialState: function () {
         return {
-            brcakey: "BRCA1",
+            brcakey: "BRCA1Path",
             data: [],
         };
     },
@@ -183,9 +218,15 @@ var Lollipop = React.createClass({
             <Grid>
                 <div>
                     <Row style={{marginBottom: '2px', marginTop: '2px'}}>
-                        <Nav bsStyle="tabs" eventKey={0} activeKey={this.state.brcakey} onSelect={this.onSelect} title="Select Gene" id="bg-vertical-dropdown-1">
-                            <NavItem eventKey="BRCA1">BRCA1</NavItem>
-                            <NavItem eventKey="BRCA2">BRCA2</NavItem>
+                        <Nav bsStyle="tabs" activeKey={this.state.brcakey} onSelect={this.onSelect} id="bg-vertical-dropdown-1">
+                            <DropdownButton bsStyle="tabs" eventKey="BRCA1Path" title="BRCA1" id="brca1-dropdown">
+                                <MenuItem eventKey="BRCA1Path">Pathogenicity</MenuItem>
+                                <MenuItem eventKey="BRCA1Allele">Allele Frequency</MenuItem>
+                            </DropdownButton>
+                            <DropdownButton bsStyle="tabs" eventKey="BRCA2Path" title="BRCA2" id="brca2-dropdown">
+                                <MenuItem eventKey="BRCA2Path">Pathogenicity</MenuItem>
+                                <MenuItem eventKey="BRCA2Allele">Allele Frequency</MenuItem>
+                            </DropdownButton>
                         </Nav>
                         <span onClick={() => this.props.onHeaderClick('Lollipop Plots')}/>
                         <D3Lollipop data={this.state.data} opts={this.props.opts} key={this.state.brcakey} brcakey={this.state.brcakey} onRowClick={this.props.onRowClick} id='brcaLollipop' ref='d3svgBrca'/>
