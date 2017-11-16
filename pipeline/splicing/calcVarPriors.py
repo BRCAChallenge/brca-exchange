2390f1aad01aba911dc2c3fcc186d05d4dbfbc68
Chloe Dlott <ccdlott@gmail.com>
2017-11-16 19:14:32 +0000

    Removes location functions (will use Ensembl API instead) and adds argument option for domain boundaries

--- pipeline/splicing/calcVarPriors.py
+++ pipeline/splicing/calcVarPriors.py
@@ -9,8 +9,6 @@ calculates either the prior probability of pathogenicity or a prior ENGIMA class
 
 import argparse
 import csv
-import re
-from calcMaxEntScanMeanStd import fetch_gene_coordinates
 
 
 def checkSequence(sequence):
@@ -30,6 +28,18 @@ def checkSequence(sequence):
         # len(sequence) = 0
         acceptableSequence = False
     return acceptableSequence
+
+def getVarStrand(variant):
+    '''Given a variant, returns the coding strand based on the variant's gene_symbol'''
+    varGene = variant["Gene_Symbol"]
+    if varGene == "BRCA1": 
+        varStrand = '-'
+    elif varGene == "BRCA2":
+        varStrand = '+'
+    else:
+        # varGene not BRCA1 or BRCA2
+        varStrand = ""
+    return varStrand
 
 
 def getVarType(variant):
@@ -67,95 +77,10 @@ def getVarType(variant):
     return varType
 
 
-def getExonBoundaries(transcript):
-    '''
-    Given a transcript, returns a dictionary containing the exon number as the key and a tuple containing the exon start and exon end as the key for each exon
-    Uses fetch_gene_coordinates from calcMaxEntScanMeanStd to get information for a particular transcript
-    '''
-    varTranscript = fetch_gene_coordinates(str(transcript))
-    # parse exon starts and exon ends
-    varTranscript['exonStarts'] = re.sub(",(\s)*$", "", varTranscript['exonStarts'])
-    varTranscript['exonEnds'] = re.sub(",(\s)*$", "", varTranscript['exonEnds'])
-    if varTranscript['strand'] == "+":
-        exonStarts = varTranscript['exonStarts'].split(',')
-        exonEnds = varTranscript['exonEnds'].split(',')
-    else:
-        # variant is on '-' strand
-        exonStarts = list(reversed(varTranscript['exonEnds'].split(',')))
-        exonEnds = list(reversed(varTranscript['exonStarts'].split(',')))
-    varExons = {}
-    varExonCount = varTranscript['exonCount']
-    exonCount = 1
-    while exonCount <= varExonCount:
-        exonStart = int(exonStarts[exonCount-1])
-        exonEnd = int(exonEnds[exonCount-1])
-        exonName = "exon" + str(exonCount)
-        varExons[exonName] = {"exonStart":exonStart,
-                              "exonEnd":exonEnd}
-        exonCount += 1
-    return varExons
-
-def varOutsideBoundaries(variant):
-    '''
-    Given a variant, determines if variant genomic position is outside transcript boundaries, handles variants on positive and negative strand
-    Returns either True (outside boundaries) or False (inside boundaries)
-    '''
-    varGenPos = int(variant['Pos'])
-    varRefTranscript = variant["Reference_Sequence"]
-    varTranscript = fetch_gene_coordinates(str(varRefTranscript))
-    varStrand = varTranscript['strand']
-    outsideBoundaries = False
-    if varStrand == '+':
-        txnStart = int(varTranscript['txStart'])
-        txnEnd = int(varTranscript['txEnd'])
-        if varGenPos < txnStart or varGenPos > txnEnd:
-            outsideBoundaries = True
-    else:
-        # varStrand == '-'
-        txnStart = int(varTranscript['txEnd'])
-        txnEnd = int(varTranscript['txStart'])
-        if varGenPos > txnStart or varGenPos < txnEnd:
-            outsideBoundaries = True
-    return outsideBoundaries
-    
-def varInExon(variant):
-    '''
-    Given a variant, determines if variant genomic position is in an exon, handles variants on positive and negative strand
-    Returns either True (in an exon) or False (not in an exon)
-    '''
-    varGenPos = int(variant["Pos"])
-    varRefTranscript = variant["Reference_Sequence"]
-    varExons = getExonBoundaries(str(varRefTranscript))
-    varTranscript = fetch_gene_coordinates(str(varRefTranscript))
-    varStrand = varTranscript["strand"]
-    inExon = False
-    for exon in varExons:
-        exonBounds = varExons[exon]
-        exonStart = int(exonBounds['exonStart'])
-        exonEnd = int(exonBounds['exonEnd'])
-        if varStrand == '+':
-            if varGenPos >= exonStart and varGenPos <= exonEnd:
-                inExon = True
-        else:
-            # varStrand == '-'
-            if varGenPos <= exonStart and varGenPos >= exonEnd:
-                inExon = True
-    return inExon
-
-
 def getVarLocation(variant):
-    '''Given a variant, returns general location of variant (outside transcript boundaries, in an exon, or an intron)'''
-    varOutsideBounds = varOutsideBoundaries(variant)
-    varExon = varInExon(variant)
-    if varOutsideBounds == True:
-        varLoc = "outsideBoundaries"
-    else:
-        # varOutsideBoundaries == False
-        if varExon == True:
-            varLoc = "inExon"
-        else:
-            # inExon == False
-            varLoc = "inIntron"
+    '''Given a variant, returns location of variant using Ensembl API for variant annotation'''
+    # TO DO - Implement this function using Ensembl API so that variant location is identified
+    varLoc = '-'
     return varLoc
 
 
@@ -168,11 +93,7 @@ def getVarDict(variant):
     varHGVS = variant["pyhgvs_cDNA"]
     varGene = variant["Gene_Symbol"]
     varChrom = variant["Chr"]
-    if varGene == "BRCA1":
-        varStrand = '-'
-    else:
-        # varGene == "BRCA2"
-        varStrand = '+'
+    varStrand = getVarStrand(variant)
     varGenCoordinate = variant["Pos"]
     varType = getVarType(variant)
     varLoc = getVarLocation(variant)
@@ -189,12 +110,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', "--inputFile", default="built.tsv", help="File with variant information")
     parser.add_argument('-o', "--outputFile", help="File where results will be output")
+    parser.add_arguement('-b', "--boundaries", default="ENIGMA", help="Specifies which boundaries (either ENIGMA or PRIORS) to use for clinically important domains")
     args = parser.parse_args()
     
     inputData = csv.DictReader(open(args.inputFile, "r"), delimiter="\t")
     for variant in inputData:
         varDict = getVarDict(variant)
-    
+
+    # TO DO - create conditional to account for user selected boundaries
     newColumns = ["varType", "varLoc", "pathProb", "ENIGMAClass", "donorVarMES", "donorVarZ", "donorRefMES", "donorRefZ", "accVarMES", "accVarZ", "accRefMES", "accRefZ", "deNovoMES", "deNovoZ", "spliceSite", "spliceRescue", "frameshift", "CNV", "spliceFlag"]
     # TO DO - create built_with_priors (copy of built) and append new columns
     
