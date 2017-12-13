195ecc482f314427ef4b7cc87ee2b88fa420c0d0
ccdlott <ccdlott@gmail.com>
2017-12-12 18:24:00 -0800

    Makes changes requested by zfisch

--- pipeline/splicing/calcVarPriors.py
+++ pipeline/splicing/calcVarPriors.py
@@ -19,6 +19,9 @@ import re
 BRCA1_CANONICAL = "ENST00000357654"
 BRCA2_CANONICAL = "ENST00000380152"
 
+# Rest Ensembl server
+SERVER = "http://rest.ensembl.org"
+
 def checkSequence(sequence):
     '''Checks if a given sequence contains acceptable nucleotides returns True if sequence is comprised entirely of acceptable bases'''
     acceptableBases = ["A", "C", "T", "G", "N", "R", "Y"]
@@ -38,7 +41,7 @@ def getVarStrand(variant):
     if varGene == "BRCA1": 
         return '-'
     elif varGene == "BRCA2":
-         return '+'
+        return '+'
     else:
         return ""
 
@@ -66,8 +69,6 @@ def getVarConsequences(variant):
     using variant chromosome, Hg38 start, Hg38 end, and alternate allele as input for API
     returns a string detailing consequences of variant
     '''
-
-    server = "http://rest.ensembl.org"
     ext = "/vep/human/region/"
 
     # varStrand always 1 because all alternate alleles and positions refer to the plus strand
@@ -85,7 +86,7 @@ def getVarConsequences(variant):
         query = "%s:%s-%s:%s/%s?" % (variant["Chr"], variant["Hg38_Start"],
                                      variant["Hg38_End"], varStrand, varAlt)
     
-        req_url = server+ext+query
+        req_url = SERVER+ext+query
         jsonOutput = _make_request(req_url)
     
         assert(len(jsonOutput) == 1)
@@ -177,7 +178,6 @@ def main():
     inputData = csv.DictReader(open(args.inputFile, "r"), delimiter="\t")
     for variant in inputData:
         varDict = getVarDict(variant)
-        
     # TO DO - create conditional to account for user selected boundaries
     newColumns = ["varType", "varLoc", "pathProb", "ENIGMAClass", "donorVarMES",
                   "donorVarZ", "donorRefMES", "donorRefZ", "accVarMES", "accVarZ",
