dc47e1d85b852afeb71809321db4debe858e5157
Zack Fischmann <zacharyrobert@gmail.com>
2017-12-14 15:12:03 -0800

    create faq page with slugified faq questions

--- website/js/index.js
+++ website/js/index.js
@@ -19,6 +19,7 @@ require('css/custom.css');
 var _ = require('underscore');
 var backend = require('./backend');
 var {NavBarNew} = require('./NavBarNew');
+var FAQ = require('./Faq');
 var Rx = require('rx');
 require('rx-dom');
 var moment = require('moment');
@@ -226,6 +227,7 @@ var Help = React.createClass({
         );
     }
 });
+
 
 function toNumber(v) {
     return _.isString(v) ? parseInt(v) : v;
@@ -1047,6 +1049,7 @@ var routes = (
         <Route path='factsheet' handler={FactSheet}/>
         <Route path='help' handler={Help}/>
         <Route path='community' handler={Community}/>
+        <Route path='faq' handler={FAQ}/>
         <Route path='signup' handler={Signup}/>
         <Route path='signin' handler={Signin}/>
         <Route path='mailinglist' handler={MailingList}/>
