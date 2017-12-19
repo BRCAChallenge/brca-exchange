c9a6a81ed2d80ce333d0d70abe83c0d4834e0a52
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2017-12-19 21:42:02 +0100

    adding possibility to set date explicitely in docker pipeline (#684)
    also adding vi to docker image for convenience

--- pipeline/docker/run_luigi.sh
+++ pipeline/docker/run_luigi.sh
@@ -2,6 +2,11 @@
 
 # note: all paths are valid within the docker container!
 
+# if DATA_DATE is set as environment variable, set the pipeline date explicitely (useful for continuing a pipeline run on another day)
+if [ -n "${DATA_DATE}" ]; then
+    DATE_PARAM_OPT="--date ${DATA_DATE}"
+fi
+
 OUTPUT_DIR=/files/data/output
 PARENT_DIR=/files/data
 BRCA_RESOURCES=/files/resources
@@ -20,4 +25,4 @@ echo "Git hash: $(git log | head -n 1)"
 
 cd /opt/brca-exchange/pipeline/luigi
 
-python -m luigi --logging-conf-file luigi_log_configuration.conf --module CompileVCFFiles RunAll --resources-dir ${BRCA_RESOURCES} --file-parent-dir ${PARENT_DIR} --output-dir ${OUTPUT_DIR} --previous-release-tar ${PREVIOUS_RELEASE_TAR} --release-notes ${RELEASE_NOTES} --local-scheduler
+python -m luigi --logging-conf-file luigi_log_configuration.conf --module CompileVCFFiles RunAll --resources-dir ${BRCA_RESOURCES} --file-parent-dir ${PARENT_DIR} --output-dir ${OUTPUT_DIR} --previous-release-tar ${PREVIOUS_RELEASE_TAR} --release-notes ${RELEASE_NOTES} ${DATE_PARAM_OPT} --local-scheduler
