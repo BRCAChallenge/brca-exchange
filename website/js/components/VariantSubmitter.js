b8cbe8f58f5c6e9ab94ca009f2a35e8d59c2835c
Faisal <falquaddoomi@ucla.edu>
2018-01-04 14:12:05 -0800

    Changed report header from a table-based to flex-based layout, which appears to improve performance. Migrated most of the inline styles into custom.css.

--- website/js/components/VariantSubmitter.js
+++ website/js/components/VariantSubmitter.js
@@ -12,14 +12,6 @@ function sentenceCase(str) {
     return str.replace(/\b\S/g, (t) => t.toUpperCase() );
 }
 
-const ellipsizedStyle = {
-    fontWeight: 'bold',
-    textOverflow: 'ellipsis',
-    /* Required for text-overflow to do anything */
-    whiteSpace: 'nowrap',
-    overflow: 'hidden'
-};
-
 // from https://www.ncbi.nlm.nih.gov/clinvar/docs/review_status/#revstat_def
 const starsToRevstatuses = {
     4: ['practice_guideline'],
@@ -78,31 +70,63 @@ const VariantSubmitter = React.createClass({
     },
 
     // source-specific headers
-    headerComponent: {
-        'ClinVar': function(data) {
+    generateHeader: function(source, submitter, data) {
+        let extraHeaderItems = null;
+
+        if (source === 'ClinVar') {
             const significance = util.getFormattedFieldByProp("Clinical_Significance_ClinVar", data);
             const dateUpdated = util.getFormattedFieldByProp("Date_Last_Updated_ClinVar", data);
 
-            return (
-                <div style={{display: 'flex'}}>
-                    <div style={{flex: '1 1 auto', textAlign: 'left'}}>
-                    {`${sentenceCase(significance)} (${dateUpdated})`}
-                    </div>
-                    <div style={{flex: '0 1 auto', textAlign: 'right', whiteSpace: 'nowrap'}}>
-                    { getStarsForReviewStatus(data['Review_Status_ClinVar']) }
-                    </div>
+            extraHeaderItems = (
+                <div>
+                {`${sentenceCase(significance)} (${dateUpdated})`}
                 </div>
             );
-        },
-        'LOVD': function(data) {
+        }
+        else if (source === 'LOVD') {
             const variantEffect = util.getFormattedFieldByProp("Variant_effect_LOVD", data);
 
-            return (
-                <div style={{textAlign: 'left'}}>
+            extraHeaderItems = (
+                <div>
                 {`Variant Effect: ${variantEffect}`}
                 </div>
             );
         }
+
+        return (
+            <div className={`submitter-header ${this.state.expanded ? 'expanded' : ''}`} onClick={this.onHandleToggle}>
+                <div className="submitter-cell submitter-label">
+                    {
+                        // for ClinVar reports, display stars to the left of the submitter label
+                        (this.props.source === 'ClinVar')
+                            ? getStarsForReviewStatus(data['Review_Status_ClinVar'])
+                            : null
+                    }
+
+                    {
+                        this.state.expanded
+                            ? <i className="fa fa-caret-down" aria-hidden="true" />
+                            : <i className="fa fa-caret-right" aria-hidden="true" />
+                    }
+                    &nbsp;
+                    <span>{this.props.meta.submitter.title}</span>
+                </div>
+
+                <div
+                    className={`submitter-cell submitter-name ${!this.state.expanded ? 'collapsed' : ''}`}
+                    style={{textAlign: 'left', flex: '1 1 auto'}}
+                >
+                { submitter }
+                </div>
+
+                <div className="submitter-cell" style={{textAlign: 'left', flex: '0 1 auto', whiteSpace: 'nowrap'}}>
+                {
+                    // remaining header elements depend on the source
+                    extraHeaderItems
+                }
+                </div>
+            </div>
+        );
     },
 
     render: function() {
@@ -126,32 +150,14 @@ const VariantSubmitter = React.createClass({
             );
         });
 
-        // FIXME: explain why we have two tables, one for the header and one for the collapsible contents
-
         return (
             <div>
-                <Table style={{marginBottom: 0, borderTop: 'solid 2px #ccc'}}>
-                    <thead>
-                        <tr className={`submitter-header ${this.state.expanded ? 'expanded' : ''}`} onClick={this.onHandleToggle}>
-                            <td className='help-target'>
-                                {
-                                    this.state.expanded
-                                        ? <i className="fa fa-caret-down" aria-hidden="true" />
-                                        : <i className="fa fa-caret-right" aria-hidden="true" />
-                                }
-                                &nbsp;
-                                <span>{this.props.meta.submitter.title}:</span>
-                            </td>
-                            <td colSpan={2} style={!this.state.expanded ? ellipsizedStyle : {}}>{ submitter }</td>
-                            <td>
-                            {
-                                // remaining header elements depend on the source
-                                this.headerComponent[this.props.source](data)
-                            }
-                            </td>
-                        </tr>
-                    </thead>
-                </Table>
+                <div style={{marginBottom: 0, borderTop: 'solid 2px #ccc'}}>
+                {
+                    // remaining header elements depend on the source
+                    this.generateHeader(this.props.source, submitter, data)
+                }
+                </div>
 
                 <div ref='panel' className={classNames(styles)}>
                     <Table key={`submitter-name-${submitter}`}>
