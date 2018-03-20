cc22a23bdf381f5175fa7aedfdc77d6e970ceb50
Zack Fischmann <zacharyrobert@gmail.com>
2018-03-19 20:55:38 -0700

    link to position on mupit structure

--- website/js/MupitStructure.js
+++ website/js/MupitStructure.js
@@ -16,9 +16,11 @@ class MupitStructure extends React.Component {
     render() {
         let {variant, prop} = this.props;
         let mupitStructure = _.find(content.mupitStructures, function(structure) {return structure.name === variant[prop].name;});
+        debugger;
+        let mupitUrl = mupitStructure.url + "&gm=chr" + variant.Chr + ":" + variant.Pos + "&altaa=" + variant.Alt.toLowerCase();
         return (
             <div className="mupit-structure">
-                <a href={mupitStructure.url} target="_blank">
+                <a href={mupitUrl} target="_blank">
                     <img src={mupitStructure.image}></img>
                 </a>
             </div>
