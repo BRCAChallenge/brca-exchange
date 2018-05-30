d072113b0632053acb6cce0ccfcacf460c101e8d
Faisal <falquaddoomi@ucla.edu>
2018-05-30 13:03:06 +0200

    KeyInline now displays a tooltip on hover if one is provided via the tooltip prompt.

--- website/js/components/KeyInline.js
+++ website/js/components/KeyInline.js
@@ -3,15 +3,34 @@
 'use strict';
 
 import React from "react";
+import {OverlayTrigger, Popover} from "react-bootstrap";
 
 const KeyInline = React.createClass({
     render() {
-        const {onClick, tableKey} = this.props;
-        return (
-            <td className='help-target'>
-                <span className="help-target-inline" onClick={onClick}>{tableKey}</span>
-            </td>
-        );
+        const {onClick, tableKey, tooltip} = this.props;
+
+        if (tooltip) {
+            const popper = (
+                <Popover title='Quick Definition'>
+                    <span dangerouslySetInnerHTML={{__html: tooltip}} />
+                </Popover>
+            );
+
+            return (
+                <td className='help-target'>
+                    <OverlayTrigger placement='right' overlay={popper}>
+                        <span className="help-target-inline" onClick={onClick}>{tableKey}</span>
+                    </OverlayTrigger>
+                </td>
+            );
+        }
+        else {
+            return (
+                <td className='help-target'>
+                    <span className="help-target-inline" onClick={onClick}>{tableKey}</span>
+                </td>
+            );
+        }
     }
 });
 
