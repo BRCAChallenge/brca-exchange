a3a0dbe2732621b2e777647776fd1a28026aba4a
Faisal <falquaddoomi@ucla.edu>
2018-06-15 13:33:52 +0200

    Moved "prior to considering other evidence" into tile header text to save space.

--- website/js/VariantTable.js
+++ website/js/VariantTable.js
@@ -233,7 +233,7 @@ const researchModeGroups = [
         {title: 'Literature Reference', prop: 'Literature_source_exLOVD', core: true}
     ]},
 
-    {groupTitle: 'In Silico Prediction', internalGroupName: 'InSilicoPrediction', inSilicoPred: true,
+    {groupTitle: 'In Silico Prediction (prior to considering other evidence)', internalGroupName: 'InSilicoPrediction', inSilicoPred: true,
         hideFromColumnSelection: true,
         innerCols: []
     },
