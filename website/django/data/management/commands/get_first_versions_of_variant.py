510df0470321eeba10f5698b9b79eb226c8f44b2
Zack Fischmann <zacharyrobert@gmail.com>
2018-06-21 15:49:12 -0700

    exclude variants added in first release

--- website/django/data/management/commands/get_first_versions_of_variant.py
+++ website/django/data/management/commands/get_first_versions_of_variant.py
@@ -1,6 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
+<<<<<<< Updated upstream
 from data.models import Variant
+=======
+from data.models import Variant, CurrentVariant
+>>>>>>> Stashed changes
 from django.db import transaction
 import csv
 import pdb
@@ -11,16 +15,27 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        seen = {}
-        with open('/tmp/firstVersions.csv', 'wb') as csvfile:
-            fieldnames = ['Genomic_Coordinate_hg38', 'Data_Release_id', 'Only_submitted_by_ENIGMA']
+        seen = []
+        current_variant_keys = []
+        with open('/tmp/first_versions_of_variants_barring_first_release.csv', 'wb') as csvfile:
+            fieldnames = ['Genomic_Coordinate_hg38', 'Gene_Symbol', 'HGVS_cDNA', 'Data_Release_id', 'Only_submitted_by_ENIGMA']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
-            variants = Variant.objects.all().exclude(Data_Release_id='1').order_by("Data_Release_id")
+            variants = Variant.objects.all().order_by("Data_Release_id")
+            current_variants = CurrentVariant.objects.all().exclude(Change_Type__name='deleted')
+            for current_variant in current_variants:
+                current_variant_keys.append(current_variant.Genomic_Coordinate_hg38)
             for variant in variants:
                 key = variant.Genomic_Coordinate_hg38
-                if key not in seen:
-                    seen[key] = 1
-                    writer.writerow({'Genomic_Coordinate_hg38': key, 'Data_Release_id': variant.Data_Release_id, 'Only_submitted_by_ENIGMA': variant.Source == "ENIGMA"})
+                if key in current_variant_keys and key not in seen:
+                    seen.append(key)
+                    if variant.Data_Release_id > 1:
+                        writer.writerow({
+                            'Genomic_Coordinate_hg38': key,
+                            'Gene_Symbol': variant.Gene_Symbol,
+                            'HGVS_cDNA': variant.HGVS_cDNA,
+                            'Data_Release_id': variant.Data_Release_id,
+                            'Only_submitted_by_ENIGMA': variant.Source == "ENIGMA"
+                        })
 
         print "Done!"
