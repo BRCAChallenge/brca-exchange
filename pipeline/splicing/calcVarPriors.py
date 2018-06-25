1947afbeb6b0b85f95efcef0be580655b0b70ebd
Rob Currie <rcurrie@ucsc.edu>
2018-06-24 19:46:04 -0700

    Integrated references download, test and calc into Docker. Add detailed documentation in README. Refine reference download and install.

--- pipeline/splicing/calcVarPriors.py
+++ pipeline/splicing/calcVarPriors.py
@@ -17,6 +17,7 @@ import re
 import subprocess
 import tempfile
 import os
+import pytest
 import pyhgvs
 import pyhgvs.utils as pyhgvs_utils
 from pygr.seqdb import SequenceFileDB
@@ -3476,17 +3477,7 @@ def getVarDict(variant, boundaries):
 
     return varDict
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', "--inputFile", default="built.tsv", help="File with variant information")
-    parser.add_argument('-o', "--outputFile", help="File where results will be output")
-    parser.add_argument('-v', "--variantFile", help="File containing protein priors for variants")
-    parser.add_argument('-b', "--boundaries", default="enigma",
-                        help="Specifies which boundaries ('enigma' or 'priors') to use for clinically important domains")
-    parser.add_argument('-g', "--genomeFile", help="Fasta file containing hg38 reference genome")
-    parser.add_argument('-t', "--transcriptFile", help="RefSeq annotation hg38-based genepred file")
-    args = parser.parse_args()    
-
+def calc(args):
     inputData = csv.DictReader(open(args.inputFile, "r"), delimiter="\t")
     fieldnames = inputData.fieldnames
     newHeaders = open("headers.tsv", "r").read().split()
@@ -3519,6 +3510,51 @@ def main():
         outputData.writerow(variant)
         totalVariants += 1
         print "variant", totalVariants, "complete"
+
+def run(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    while True:
+        line = process.stdout.readline().rstrip()
+        if line:
+            print(line)
+        else:
+            break
     
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Calculates splice site priors from a list of variants")
+    parser.add_argument("command", 
+                        help="references, test short, test long or calc")
+
+    parser.add_argument("inputFile", nargs="?", help="Input variant file")
+    parser.add_argument("outputFile", nargs="?", help="Output priors file")
+
+    parser.add_argument('-v', "--variantFile", default="mod_res_dn_brca20160525.txt",
+                        help="File containing protein priors for variants")
+    parser.add_argument('-b', "--boundaries", default="enigma",
+                        help="Boundaries ('enigma' or 'priors') to use for clinically important domains")
+    parser.add_argument('-g', "--genomeFile", default="/references/hg38.fa",
+                        help="Fasta file containing hg38 reference genome")
+    parser.add_argument('-t', "--transcriptFile", default="refseq_annotation.hg38.gp", 
+                        help="RefSeq annotation hg38-based genepred file")
+    args = parser.parse_args() 
+
+    # REMIND: Hack way to parts commands, should use sub-argument parser
+    if args.command == "references":
+        run("./references.sh")
+    elif args.command == "test" and args.inputFile == "short":
+        pytest.main(["-x", "."])
+        args.inputFile = "tests/variants_short.tsv"
+        args.outputFile = "/tmp/priors_short.tsv"
+        calc(args)
+        run("md5sum -c ./tests/md5/priors_short.md5")
+    elif args.command == "test" and args.inputFile == "long":
+        pytest.main(["-x", "."])
+        args.inputFile = "tests/variants_long.tsv"
+        args.outputFile = "/tmp/priors_long.tsv"
+        calc(args)
+        run("md5sum -c ./tests/md5/priors_long.md5")
+    elif args.command == "calc" and (args.inputFile and args.outputFile):
+        calc(args)
+    else:
+        parser.print_help()
