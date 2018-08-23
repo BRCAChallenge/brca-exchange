ca74ab5938bac54992ad70a20b914ece372f45ca
Faisal <falquaddoomi@ucla.edu>
2018-08-23 11:47:41 +0200

    Restored database reference to original (used to be a testing db), restored backend URL to production.

--- website/django/brca/site_settings.py
+++ website/django/brca/site_settings.py
@@ -22,7 +22,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': 'localhost',
-        'NAME': 'storage_priors',
+        'NAME': 'storage.pg',
         'USER': 'postgres',
         'PASSWORD': 'postgres'
     }
