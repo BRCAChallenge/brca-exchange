52cd35757d967cf12494345cb85cacd91bea610d
Zack Fischmann <zacharyrobert@gmail.com>
2018-08-27 17:06:35 -0700

    update functional analysis migration to fix migration conflicts

--- website/django/data/migrations/0034_add_functional_analysis_fields.py
+++ website/django/data/migrations/0034_add_functional_analysis_fields.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data', '0032_additional_clinvar_fields'),
+        ('data', '0033_report_synonyms_clinvar'),
     ]
 
     operations = [
