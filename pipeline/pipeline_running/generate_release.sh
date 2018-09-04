b2b4eecc09b693fda571dde2dd99f0843b9c0e79
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2018-09-04 09:58:20 +0200

    WIP various fixes

--- pipeline/pipeline_running/generate_release.sh
+++ pipeline/pipeline_running/generate_release.sh
@@ -5,6 +5,9 @@
 
 ROOT_DIR=$(realpath $1)
 CREDENTIALS_PATH=$(realpath $2)
+PREVIOUS_RELEASE_PATH=$(realpath $3)
+
+GIT_COMMIT=${4-:"master"}
 
 DATA_DATE="$(date +%Y-%m-%d)"
 WDIR="${ROOT_DIR}/data_release_${DATA_DATE}"
@@ -13,6 +16,7 @@ mkdir -p ${WDIR}
 
 CODE_BASE="${WDIR}/code"
 [ -d ${CODE_BASE} ] || git clone https://github.com/BRCAChallenge/brca-exchange.git ${CODE_BASE}
+cd ${CODE_BASE} && git checkout ${GIT_COMMIT}
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -21,6 +25,8 @@ CFG_PATH=${CODE_BASE}/pipeline/brca_pipeline_cfg.mk
 jinja2 -D DATA_DATE="${DATA_DATE}" -D WORK_DIR="${WDIR}" \
        -D CODE_BASE="${CODE_BASE}" \
        -D CREDENTIALS_PATH="${CREDENTIALS_PATH}" \
+       -D PREVIOUS_RELEASE_PATH="${PREVIOUS_RELEASE_PATH}" \
+       -D GIT_COMMIT="${GIT_COMMIT}" \
        "${CODE_BASE}/pipeline/pipeline_running/brca_pipeline_cfg.mk.j2" > ${CFG_PATH}
 
 cd "${CODE_BASE}/pipeline"
