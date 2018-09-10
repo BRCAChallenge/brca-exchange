b5a4a03ffef795a12207bb2e83f2763cee02c564
Zack Fischmann <zacharyrobert@gmail.com>
2018-09-10 14:58:46 -0700

    update lovd migration

--- website/django/data/migrations/0035_add_functional_analysis_fields.py
+++ website/django/data/migrations/0035_add_functional_analysis_fields.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data', '0033_report_synonyms_clinvar'),
+        ('data', '0034_insilicopriors'),
     ]
 
     operations = [
