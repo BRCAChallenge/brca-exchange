20d8eaa77bfae0042d9f1013c9e1fccf75372eb6
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2018-10-19 08:55:54 +0200

    small fix in filter_clinvar_brca and some more comments there

--- pipeline/clinvar/filter_clinvar_brca.py
+++ pipeline/clinvar/filter_clinvar_brca.py
@@ -14,31 +14,34 @@ def open_maybe_gzip(fname):
     if fname.endswith('gz'):
         return gzip.open(fname, 'rb')
     else:
-        return open(fname)
+        return gzip.GzipFile(fname)
 
 
-def filter_engima_xml(fin, fout):
+def filter_xml(fin, fout):
     fout = open(fout, 'w')
 
+    # copying ClinVarSet if it contains a Symbol with a "Preferred" ElementValue BRCA*
     xpath_filter = 'ReferenceClinVarAssertion/MeasureSet/Measure/' \
                    'MeasureRelationship/Symbol[starts-with(ElementValue, "BRCA") and ElementValue/@Type="Preferred"]'
 
-    # writing header
+    # copying header
     with open_maybe_gzip(fin) as f:
         header_lines = itertools.takewhile(lambda s: "ClinVarSet" not in s, f)
         fout.writelines(header_lines)
 
     # filtering ClinVarSet's we are interested in
-    for _, el in etree.iterparse(gzip.GzipFile(fin), tag='ClinVarSet'):
-        if len(el.xpath(xpath_filter)) >= 1:
-            fout.write(etree.tostring(el, pretty_print=True))
+    with open_maybe_gzip(fin) as f:
+        for _, el in etree.iterparse(f, tag='ClinVarSet'):
+            if len(el.xpath(xpath_filter)) >= 1:
+                fout.write(etree.tostring(el, pretty_print=True))
 
-        # some cleanup to save a significant amount of memory (inspired by https://www.ibm.com/developerworks/xml/library/x-hiperfparse/)
-        el.clear()
-        # Also eliminate now-empty references from the root node to elem
-        for ancestor in el.xpath('ancestor-or-self::*'):
-            while ancestor.getprevious() is not None:
-                del ancestor.getparent()[0]
+            # some cleanup to save a significant amount of memory
+            # (inspired by https://www.ibm.com/developerworks/xml/library/x-hiperfparse/)
+            el.clear()
+            # Also eliminate now-empty references from the root node to elem
+            for ancestor in el.xpath('ancestor-or-self::*'):
+                while ancestor.getprevious() is not None:
+                    del ancestor.getparent()[0]
 
     # writing "footer"
     fout.write("</ReleaseSet>")
@@ -50,7 +53,7 @@ def filter_engima_xml(fin, fout):
 @click.argument('input', type=click.Path(exists=True))
 @click.argument('output', type=click.Path())
 def main(input, output):
-    filter_engima_xml(input, output)
+    filter_xml(input, output)
 
 
 if __name__ == "__main__":
