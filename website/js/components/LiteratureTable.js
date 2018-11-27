93f9cc356e8bc00bdce5429fd2cd9b186c6971c3
Zack Fischmann <zacharyrobert@gmail.com>
2018-11-27 13:02:47 -0800

    satisfy linter

--- website/js/components/LiteratureTable.js
+++ website/js/components/LiteratureTable.js
@@ -149,7 +149,7 @@ class LiteratureTable extends React.Component {
     }
 
     render() {
-        if (!this.state.papers || this.state.papers.length == 0) {
+        if (!this.state.papers || this.state.papers.length === 0) {
             //empty or does not exist
             return (<div />);
         }
