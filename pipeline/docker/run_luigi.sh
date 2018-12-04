9dd2d4723d71432649b63c9c0e0732dd736b640b
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2018-12-04 14:27:41 +0100

    adding new Makefile targets: force-run-task, clean-files-from, show-luigi-graph tweaked luigi graph and options handling

--- pipeline/docker/run_luigi.sh
+++ pipeline/docker/run_luigi.sh
@@ -2,7 +2,7 @@
 
 # if DATA_DATE is set as environment variable, set the pipeline date explicitely (useful for continuing a pipeline run on another day)
 if [ -n "${DATA_DATE}" ]; then
-    DATE_PARAM_OPT="--date ${DATA_DATE}"
+    DATE_PARAM_OPT="--BRCAParams-date ${DATA_DATE}"
 fi
 
 # note: paths are valid within the docker container!
@@ -10,16 +10,15 @@ PARENT_DIR=/files/data
 OUTPUT_DIR="${PARENT_DIR}/output"
 BRCA_RESOURCES=/files/resources
 
-# TODO doc why different mechanism
-if [ "$#" -lt "3" ]; then
-    echo "Require host (!) paths for references directory and data directory"
-    echo "Usage: run_luigi.sh [PRIORS_REFERENCES] [OUTPUT_DIR_HOST] [PRIORS_DOCKER_IMAGE_NAME] [TASK]" 
+if [ "$#" -lt "4" ]; then
+    echo "Usage: run_luigi.sh [PRIORS_REFERENCES] [OUTPUT_DIR_HOST] [PRIORS_DOCKER_IMAGE_NAME] [TASK] [WORKERS]" 
 fi
 
 PRIORS_REFERENCES=$1
 OUTPUT_DIR_HOST="$2/output"
 PRIORS_DOCKER_IMAGE=$3
-LUIGI_TASK=${4:-"RunAll"}
+LUIGI_TASK=${4}
+N_WORKERS=${5}
 
 PREVIOUS_RELEASE_TAR=/files/previous_release.tar.gz
 
@@ -36,4 +35,4 @@ echo "Git hash: $(git log | head -n 1)"
 cd /opt/brca-exchange/pipeline/luigi
 
 echo "Attempting to run task ${LUIGI_TASK}"
-python -m luigi --logging-conf-file luigi_log_configuration.conf --module CompileVCFFiles ${LUIGI_TASK} --resources-dir ${BRCA_RESOURCES} --file-parent-dir ${PARENT_DIR} --output-dir ${OUTPUT_DIR} --previous-release-tar ${PREVIOUS_RELEASE_TAR} --priors-references-dir ${PRIORS_REFERENCES} --priors-docker-image-name ${PRIORS_DOCKER_IMAGE} --output-dir-host ${OUTPUT_DIR_HOST} --release-notes ${RELEASE_NOTES} ${DATE_PARAM_OPT} --local-scheduler
+python -m luigi --logging-conf-file luigi_log_configuration.conf --module CompileVCFFiles ${LUIGI_TASK} --BRCAParams-resources-dir ${BRCA_RESOURCES} --BRCAParams-file-parent-dir ${PARENT_DIR} --BRCAParams-output-dir ${OUTPUT_DIR} --BRCAParams-previous-release-tar ${PREVIOUS_RELEASE_TAR} --BRCAParams-priors-references-dir ${PRIORS_REFERENCES} --BRCAParams-priors-docker-image-name ${PRIORS_DOCKER_IMAGE} --BRCAParams-output-dir-host ${OUTPUT_DIR_HOST} --BRCAParams-release-notes ${RELEASE_NOTES} ${DATE_PARAM_OPT} --workers ${N_WORKERS} --local-scheduler
