91202f78fb9fa8c56770a467189467bce3a8163b
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2019-01-23 16:08:10 +0100

    integrating local version of seqrepo into pipeline upgrading to recent versions of biocommons package going single threaded for enigma_from_clinvar as it is fast enough with local seqrepo. Also, not clear whether seqrepo can be run in a multithreaded setting

--- pipeline/clinvar/hgvs_utils.py
+++ pipeline/clinvar/hgvs_utils.py
@@ -4,7 +4,7 @@ import hgvs.dataproviders.uta
 import hgvs.assemblymapper
 
 import logging
-
+import time
 
 # TODO: does not belong here. move to utilties or similar. but then need to change how luigi stuff is ran.
 class HGVSWrapper:
@@ -14,16 +14,30 @@ class HGVSWrapper:
         self.hgvs_hp = hgvs.parser.Parser()
         self.hgvs_am = hgvs.assemblymapper.AssemblyMapper(self.hgvs_dp)
 
-
+        
     # TODO: think about interface
-    def compute_protein_change(self, hgvs_cnda, include_braces=True):
+    
+    def compute_protein_change(self, hgvs_cdna, include_braces=True, retry_cnts = 5):
+        
         try:
-            v = self.hgvs_hp.parse_hgvs_variant(hgvs_cnda)
+            v = self.hgvs_hp.parse_hgvs_variant(hgvs_cdna)
             vp = self.hgvs_am.c_to_p(v)
 
             if include_braces and vp and vp.posedit:
                 vp.posedit.uncertain = True
+        except hgvs.exceptions.HGVSDataNotAvailableError as e:
+            time.sleep(1)
+            
+            if(retry_cnts > 0):
+                logging.warn("Retrying another time for %s", hgvs_cdna)
+                
+                return self.compute_protein_change(hgvs_cdna, include_braces, retry_cnts - 1)
+            else:
+                # fail the pipeline. We actually should get data, but don't due to some intermittent issue
+                # see also https://github.com/biocommons/hgvs/issues/519
+                raise RuntimeError("HGVS data not available for %s . Cannot continue. Error was %s", hgvs_cdna, e.message)
         except Exception as e:
-            logging.warning('Issues converting %s. %s', hgvs_cnda, e.message)
+            logging.warning('Issues converting %s. %s type %s', hgvs_cdna, e.message, type(e))
             return None
+
         return vp
