1e16ba6235484f96ed474573c338aa782dd6b788
Zack Fischmann <zacharyrobert@gmail.com>
2019-02-25 14:51:18 -0800

    use rename and specify relevant columns to simplify code

--- pipeline/functional_assays/parse_functional_assay_data.py
+++ pipeline/functional_assays/parse_functional_assay_data.py
@@ -19,34 +19,32 @@ def parse_args():
     return options
 
 def extract_relevant_data_for_processing(f_in_data_frame):
-
-    new_columns = [
-        'chr',
-        'pos_hg19',
-        'ref',
+    relevant_existing_columns = [
+        'chromosome',
+        'position (hg19)',
+        'reference',
         'alt',
-        'gene_symbol',
-        'reference_sequence',
-        'hgvs_nucleotide',
-        'log_rna_depletion',
-        'functional_enrichment_score']
+        'gene',
+        'transcript_ID',
+        'transcript_variant',
+        'mean.rna.score',
+        'function.score.mean'
+    ]
 
-    new_data_frame = pd.DataFrame(columns=new_columns)
+    column_name_mapping = {
+        'chromosome': 'chr',
+        'position (hg19)': 'pos_hg19',
+        'reference': 'ref',
+        'alt': 'alt',
+        'gene': 'gene_symbol',
+        'transcript_ID': 'reference_sequence',
+        'transcript_variant': 'hgvs_nucleotide',
+        'mean.rna.score': 'log_rna_depletion',
+        'function.score.mean': 'functional_enrichment_score'
+    }
 
-    new_data_frame = new_data_frame.reset_index(drop=True)
-    f_in_data_frame = f_in_data_frame.reset_index(drop=True)
-
-    new_data_frame.chr = f_in_data_frame['chromosome']
-    new_data_frame.pos_hg19 = f_in_data_frame['position (hg19)']
-    new_data_frame.ref = f_in_data_frame['reference']
-    new_data_frame.alt = f_in_data_frame['alt']
-    new_data_frame.gene_symbol = f_in_data_frame['gene']
-    new_data_frame.reference_sequence = f_in_data_frame['transcript_ID']
-    new_data_frame.hgvs_nucleotide = f_in_data_frame['transcript_variant']
-    new_data_frame.log_rna_depletion = f_in_data_frame['mean.rna.score']
-    new_data_frame.functional_enrichment_score = f_in_data_frame['function.score.mean']
-
-    return new_data_frame
+    parsed_data_frame = f_in_data_frame[relevant_existing_columns]
+    return parsed_data_frame.rename(columns=column_name_mapping)
 
 
 def main():
