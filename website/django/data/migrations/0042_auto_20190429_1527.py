aa4e1e4cd0b6d1314018e05d06d2758653aebb18
Zack Fischmann <zacharyrobert@gmail.com>
2019-04-29 16:49:34 -0700

    recreate materialized view as part of migration

--- website/django/data/migrations/0042_auto_20190429_1527.py
+++ website/django/data/migrations/0042_auto_20190429_1527.py
@@ -372,4 +372,14 @@ class Migration(migrations.Migration):
             name='BX_ID_GnomAD',
             field=models.TextField(default=b''),
         ),
+        migrations.RunSQL(
+            """
+            DROP MATERIALIZED VIEW IF EXISTS currentvariant;
+            CREATE MATERIALIZED VIEW currentvariant AS (
+                SELECT * FROM "variant" WHERE (
+                    "id" IN ( SELECT DISTINCT ON ("Genomic_Coordinate_hg38") "id" FROM "variant" ORDER BY "Genomic_Coordinate_hg38" ASC, "Data_Release_id" DESC )
+                )
+            );
+            """
+        ),
     ]
