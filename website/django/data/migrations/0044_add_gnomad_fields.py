0f4bd074feb7522e956ed8ba522e1e22237ba780
Zack Fischmann <zacharyrobert@gmail.com>
2019-07-01 10:37:45 -0700

    reorder migrations

--- website/django/data/migrations/0044_add_gnomad_fields.py
+++ website/django/data/migrations/0044_add_gnomad_fields.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data', '0041_paper_crawl_date'),
+        ('data', '0043_variantrepresentation'),
     ]
 
     operations = [
