dfc910f91c7ab99ebfc642cb51bd7ed0cb896aaf
Zack Fischmann <zacharyrobert@gmail.com>
2019-09-04 15:58:24 -0700

    remove ga4gh api code, documentation and related imports. also removes beacon query code since this data is generated dynamically in react and the scripts arent used

--- website/django/data/urls.py
+++ website/django/data/urls.py
@@ -12,15 +12,6 @@ urlpatterns = [
     url(r'^variantpapers/$',views.variant_papers, name="variant_papers"),
     url(r'^variantreps/$', views.variantreps, name="variant_reps"),
     url(r'^sitemap.txt$', views.sitemap, name="sitemap"),
-    url(r'^suggestions/$', views.autocomplete),
-    url(r'^ga4gh/v0.6.0a7/variants/search$', views.search_variants, name='search_variants'),
-    url(r'^ga4gh/v0.6.0a7/variants/(?P<variant_id>.+)$', views.get_variant, name = 'get_variant'),
-    url(r'^ga4gh/v0.6.0a7/variantsets/search', views.search_variant_sets, name='search_variant_sets'),
-    url(r'^ga4gh/v0.6.0a7/variantsets/(?P<variant_set_id>.+)$', views.get_variant_set, name='get_variant_set'),
-    url(r'^ga4gh/v0.6.0a7/datasets/search', views.search_datasets, name='search_datasets'),
-    url(r'^ga4gh/v0.6.0a7/datasets/(?P<dataset_id>.+)$', views.get_dataset, name="get_dataset" ),
-    url(r'^ga4gh/v0.6.0a7/variantsets', views.empty_variantset_id_catcher, name='empty_variantset_id_catcher'),
-    url(r'^ga4gh/v0.6.0a7/variants', views.empty_variant_id_catcher, name='empty_variant_id_catcher'),
-    url(r'^ga4gh/v0.6.0a7/datasets', views.empty_dataset_catcher, name='empty_dataset_catcher')
+    url(r'^suggestions/$', views.autocomplete)
 
 ]
