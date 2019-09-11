c486803668e962859c45b2e1eefac5bc17af7d76
Zack Fischmann <zacharyrobert@gmail.com>
2019-09-11 11:55:06 -0700

    use virtualenv3 to not overwrite virtualenv from py2

--- deployment/copy-community-data.sh
+++ deployment/copy-community-data.sh
@@ -15,7 +15,7 @@ USER=brca
 # Copy community relevant DB data from production to beta
 ssh -l${USER} ${HOST} <<-ENDSSH
     set -o errexit
-    . /var/www/backend/beta/virtualenv/bin/activate
+    . /var/www/backend/beta/virtualenv3/bin/activate
     sudo -u postgres pg_dump -d production.pg -F c -t "^users*" -t django_admin_log -t django_content_type -t "^auth*" -c -f /tmp/users_seq.dump
     sudo -u postgres pg_restore /tmp/users_seq.dump -c -v -1 -d storage.pg
     sudo chown -R www-data:www-data /var/www/backend/beta/django/uploads
