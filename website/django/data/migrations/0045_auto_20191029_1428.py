943a9d497bee4d23af1171eb3750ead06d98a9c6
Zack Fischmann <zacharyrobert@gmail.com>
2019-10-29 13:17:22 -0700

    rebuild autocomplete table without hg36, remove final reference to hg36 from built dictionary

--- website/django/data/migrations/0045_auto_20191029_1428.py
+++ website/django/data/migrations/0045_auto_20191029_1428.py
@@ -24,6 +24,22 @@ class Migration(migrations.Migration):
             model_name='variant',
             name='Hg36_Start',
         ),
+        migrations.RunSQL("""
+            DROP TABLE IF EXISTS words;
+            CREATE TABLE words AS SELECT DISTINCT left(word, 300) as word, release_id FROM (
+            SELECT regexp_split_to_table(lower("Genomic_Coordinate_hg38"), '[\s|''"]') as word, "Data_Release_id" as release_id from variant UNION
+            SELECT regexp_split_to_table(lower("Genomic_Coordinate_hg37"), '[\s|''"]') as word, "Data_Release_id" as release_id from variant UNION
+            SELECT regexp_split_to_table(lower("Clinical_significance_ENIGMA"), '[\s|''"]') as word, "Data_Release_id" as release_id from variant UNION
+            SELECT regexp_split_to_table(lower("Gene_Symbol"), '[\s|''"]') as word, "Data_Release_id" as release_id from variant UNION
+            SELECT regexp_split_to_table(lower("Reference_Sequence"), '[\s|''"]') as word, "Data_Release_id" as release_id from variant UNION
+            SELECT regexp_split_to_table(lower("HGVS_cDNA"), '[\s|:''"]') as word, "Data_Release_id" as release_id from variant UNION
+            SELECT regexp_split_to_table(lower("BIC_Nomenclature"), '[\s|''"]') as word, "Data_Release_id" as release_id from variant UNION
+            SELECT regexp_split_to_table(lower("HGVS_Protein"), '[\s|''"]') as word, "Data_Release_id" as release_id from variant
+            )
+            AS combined_words;
+
+            CREATE INDEX words_idx ON words(word text_pattern_ops);
+        """),
         migrations.RunSQL(
             """
             DROP MATERIALIZED VIEW IF EXISTS currentvariant;
