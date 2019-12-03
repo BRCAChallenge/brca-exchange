2fddcd89d49299324a4e36052bd95d606e559c11
Zack Fischmann <zacharyrobert@gmail.com>
2019-12-03 13:06:43 -0800

    rm genomic hgvs fields from release diff

--- pipeline/utilities/releaseDiff.py
+++ pipeline/utilities/releaseDiff.py
@@ -273,7 +273,8 @@ class transformer(object):
                              "Hg37_Start", "Hg37_End", "BX_ID_ENIGMA", "BX_ID_ClinVar",
                              "BX_ID_BIC", "BX_ID_ExAC", "BX_ID_LOVD", "BX_ID_exLOVD", "BX_ID_1000_Genomes", "BX_ID_ESP",
                              "Polyphen_Prediction", "Polyphen_Score", "Sift_Prediction", "Sift_Score",
-                             "Minor_allele_frequency_ESP", "Max_Allele_Frequency", "mupit_structure"]
+                             "Minor_allele_frequency_ESP", "Max_Allele_Frequency", "mupit_structure", "Genomic_HGVS_37",
+                             "Genomic_HGVS_38"]
 
         # Header to group all logs the same variant
         variant_intro = "\n\n %s \n Old Source: %s \n New Source: %s \n\n" % (newRow[getIdentifier(newRow, isReport)],
