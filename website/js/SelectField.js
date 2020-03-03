278cea19c0ab3f5ff44786cb7340a20452304645
faisala <alquaddoomi@nexus.ethz.ch>
2020-03-03 11:42:51 +0100

    Resolves a react-bootstrap warning re using value in select controls rather than 'selected' in options.

--- website/js/SelectField.js
+++ website/js/SelectField.js
@@ -9,22 +9,24 @@ var _ = require('underscore');
 
 var SelectField = React.createClass({
     mixins: [PureRenderMixin],
+
     onChange: function (e) {
         return this.props.onChange(e.target.value);
     },
+
     render: function () {
         const {options, label, value} = this.props;
-        const opels = _.map(options, v => <option key={v} value={v} selected={v === value}>{v}</option>);
+        const opels = _.map(options, v => <option key={v} value={v}>{v}</option>);
 
         return (
             <FormGroup controlId="formControlsSelect">
                 <ControlLabel>{label}</ControlLabel>
-                <FormControl componentClass="select" onChange={this.onChange} placeholder="select">
+                <FormControl value={value} componentClass="select" onChange={this.onChange} placeholder="select">
                     {opels}
                 </FormControl>
             </FormGroup>
         );
-    }
+    },
 });
 
 module.exports = SelectField;
