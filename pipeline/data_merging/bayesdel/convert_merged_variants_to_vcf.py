e4d1fc803b5fa8235fa41287f40d25cef6d64c65
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2020-07-02 16:05:40 +0200

    cleaning up convert_merged_variants_to_vcf and improving testing

--- pipeline/data_merging/bayesdel/convert_merged_variants_to_vcf.py
+++ pipeline/data_merging/bayesdel/convert_merged_variants_to_vcf.py
@@ -1,9 +1,13 @@
-import pandas as pd
 import click
+import pandas as pd
 
-# TODO: use variant_utils?
-def sp(s):
-    return s.split(':')[0].lstrip('chr'), int(s.split(':')[1].lstrip('g.')), s, s.split(':')[2].split('>')[0], s.split(':')[2].split('>')[1]
+
+def _coord_to_vcf_cols(coord):
+    return (coord.split(':')[0].lstrip('chr'),  # chromosome col
+            int(coord.split(':')[1].lstrip('g.')),  # pos col
+            coord,  # id col
+            coord.split(':')[2].split('>')[0],  # ref col
+            coord.split(':')[2].split('>')[1])  # alt col
 
 
 def convert_merged_to_vcf(path, vcf_path):
@@ -11,25 +15,24 @@ def convert_merged_to_vcf(path, vcf_path):
 
     df = pd.read_csv(path, sep='\t')
 
-    s = df.apply(lambda d: sp(d[coord_col]), axis=1)
+    vcf_cols = df.apply(lambda d: _coord_to_vcf_cols(d[coord_col]), axis=1)
 
-    df_vcf = pd.DataFrame.from_dict(dict(zip(s.index, s.values))).T
+    df_vcf = pd.DataFrame.from_dict(dict(zip(vcf_cols.index, vcf_cols.values))).T
     df_vcf.columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT']
     df_vcf['QUAL'] = df_vcf['FILTER'] = df_vcf['ID'] = '.'
-    df_vcf['INFO'] = '.' #'Dummy1=nan;Dummy2=nan'
+    df_vcf['INFO'] = '.'
 
+    # filtering out variants with unambiguous bases
     df_vcf = df_vcf[~df_vcf['ALT'].apply(lambda s: any(x not in 'ACTG' for x in s))]
 
     with open(vcf_path, 'w') as f:
-        # TODO split string up
-        #header = '##fileformat=VCFv4.0\n##source=brcaexchange\n##reference=GRCh38\n##INFO=<ID=Dummy1,Number=.,Type=String,Description="">\n##INFO=<ID=Dummy2,Number=.,Type=String,Description="">\n'
         header = '##fileformat=VCFv4.0\n##source=brcaexchange\n##reference=GRCh38\n'
         f.write(header)
         f.write("#CHROM POS ID REF ALT QUAL FILTER INFO\n".replace(' ', '\t'))
 
-    df_vcf.to_csv(vcf_path, sep='\t', mode='a', index=False, header=None)
+    (df_vcf.sort_values(['CHROM', 'POS']).
+            to_csv(vcf_path, sep='\t', mode='a', index=False, header=None))
 
-# TODO use click or argparse?
 
 @click.command()
 @click.argument('merged_tsv', type=click.Path(readable=True))
