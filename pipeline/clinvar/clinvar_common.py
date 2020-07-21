73912a02455602faf8f79b105010117f85453666
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2020-07-21 17:25:41 +0200

    tweaking clinvar xpath expression for the gene_symbol

--- pipeline/clinvar/clinvar_common.py
+++ pipeline/clinvar/clinvar_common.py
@@ -40,11 +40,11 @@ def processClinicalSignificanceElement(el, obj):
 
 
 def build_xpath_filter_for_cv_assertions(gene_symbols):
-    symbols_str = [ f'ElementValue="{s}"' for s in gene_symbols]
+    symbols_str = [ f'text()="{s}"' for s in gene_symbols]
     symbols_pred = ' or '.join(symbols_str)
 
     # filter assertion if it contains a Symbol we are interested in
-    return f"ReferenceClinVarAssertion/MeasureSet/Measure/MeasureRelationship/Symbol[({symbols_pred}) and ElementValue/@Type=\"Preferred\"]"
+    return f"ReferenceClinVarAssertion/MeasureSet/Measure/MeasureRelationship/Symbol/ElementValue[({symbols_pred}) and @Type=\"Preferred\"]"
 
 
 def extractSynonyms(el):
