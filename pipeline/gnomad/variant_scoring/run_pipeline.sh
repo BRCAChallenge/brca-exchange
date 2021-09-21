91619dadb70a1cf336568fe5cc998f36087cabe3
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2021-09-21 15:36:01 +0200

    more comments in run_pipeline.sh

--- pipeline/gnomad/variant_scoring/run_pipeline.sh
+++ pipeline/gnomad/variant_scoring/run_pipeline.sh
@@ -3,7 +3,21 @@
 # (paths and configs with respect to dev machine)
 DATA_DIR=/data/variant_scoring/data
 
-COMMON_DOCKER_OPT=" --rm -v ${DATA_DIR}:/mnt -v /data/variant_scoring/code/pipeline:/opt/variant_scoring" 
+# the DATA_DIR is expected to contain the following
+#├── original_gnomad_data                                      # downloaded e.g. using download_data.sh
+#│   ├── gnomad.exomes.coverage.summary.tsv.bgz
+#│   ├── gnomad.exomes.r2.1.1.sites.13.vcf.bgz
+#│   ├── gnomad.exomes.r2.1.1.sites.17.vcf.bgz
+#│   ├── gnomad.genomes.r3.0.1.coverage.summary.tsv.bgz
+#│   ├── gnomad.genomes.v3.1.1.sites.chr13.vcf.bgz
+#│   └── gnomad.genomes.v3.1.1.sites.chr17.vcf.bgz
+#└── resources                                                 # taken e.g. from main brca-exchange pipeline
+#    ├── hg19ToHg38.over.chain.gz
+#    ├── hg38.fa
+
+
+
+COMMON_DOCKER_OPT=" --rm -v ${DATA_DIR}:/mnt -v /data/variant_scoring/code/pipeline:/opt/variant_scoring"
 
 docker run ${COMMON_DOCKER_OPT} variant_scoring bash -c "python /opt/variant_scoring/gnomad/variant_scoring/process_gnomad_data.py --cores 16 --mem-per-core-mb 2400 --gene-config-path /opt/variant_scoring/workflow/gene_config_brca_only.txt /mnt/original_gnomad_data /mnt/processed_brca"
 
