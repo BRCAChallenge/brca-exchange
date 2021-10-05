2eaa93bd4097a6360b551cca0afa145cd28d87dd
brca@brcaexchange-dev <brca@brcaexchange-dev.gi.ucsc.edu>
2021-10-05 12:11:47 +0000

    uncommenting instructions in run_pipeline.sh

--- pipeline/gnomad/variant_scoring/run_pipeline.sh
+++ pipeline/gnomad/variant_scoring/run_pipeline.sh
@@ -22,9 +22,9 @@ SEQ_REPO_DIR_DOCKER=/mnt/seq_repo
 
 COMMON_DOCKER_OPT=" --rm -v ${DATA_DIR}:/mnt --network host -v /data/variant_scoring/code/pipeline:/opt/variant_scoring -v /data/seqrepo:${SEQ_REPO_DIR_DOCKER} -e UTA_DB_URL=postgresql://anonymous@0.0.0.0:${UTA_PORT}/uta/uta_${UTA_RELEASE_DATE} -e HGVS_SEQREPO_DIR=${SEQ_REPO_DIR_DOCKER}/latest"
 
-#docker run ${COMMON_DOCKER_OPT} variant_scoring bash -c "python /opt/variant_scoring/gnomad/variant_scoring/process_gnomad_data.py --cores 16 --mem-per-core-mb 2400 --gene-config-path /opt/variant_scoring/workflow/gene_config_brca_only.txt /mnt/original_gnomad_data /mnt/processed_brca"
+# run process_gnomad_data.py
+docker run ${COMMON_DOCKER_OPT} variant_scoring bash -c "python /opt/variant_scoring/gnomad/variant_scoring/process_gnomad_data.py --cores 16 --mem-per-core-mb 2400 --gene-config-path /opt/variant_scoring/workflow/gene_config_brca_only.txt /mnt/original_gnomad_data /mnt/processed_brca"
 
-#docker run -it ${COMMON_DOCKER_OPT} bash
-
+# run varint_scoring.py
 docker run ${COMMON_DOCKER_OPT} variant_scoring bash -c "python /opt/variant_scoring/gnomad/variant_scoring/variant_scoring.py --gene-config-path /opt/variant_scoring/workflow/gene_config_brca_only.txt --resource-dir /mnt/resources /mnt/processed_brca /mnt/variant_scoring_df.parquet"
 
