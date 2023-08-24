d2432c4d44f550a3221b6334a310aa2a348408e9
melissacline <melissa.s.cline@gmail.com>
2023-08-24 16:02:35 -0700

    Update DonationBar.js
    Added a link to the blog article

--- website/js/components/DonationBar.js
+++ website/js/components/DonationBar.js
@@ -9,7 +9,7 @@ const DonationBar = React.createClass({
         return (
         	<div className="donation-bar">
     			<p>
-                Thank you to everyone who contributed to the BRCA Exchange user survey!  The results are available here.
+                Thank you to everyone who contributed to the BRCA Exchange user survey!  The results are available <a style={{textDecoration: 'underline'}} href="https://brcaexchange.org/blog/index.php/2023/08/20/results-of-the-brca-exchange-user-survey/">here</a> on the BRCA Exchange blog .
 	        	</p>
         	</div>
         );
