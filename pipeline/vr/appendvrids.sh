af23d27e78dd3459c067e3f67e265dce6da300f1
melissacline <melissa.s.cline@gmail.com>
2023-08-27 20:52:30 -0700

    Updated for the current versions of seqrepo-rest-api and VRS

--- pipeline/vr/appendvrids.sh
+++ pipeline/vr/appendvrids.sh
@@ -5,23 +5,23 @@ INPUT_FILE="$2"
 OUTPUT_FILE="$3"
 SEQ_REPO_DIR="${4:-/usr/local/share/seqrepo}"
 
-echo "Hello World"
-echo ${SEQ_REPO_DIR}
 
 # --- pre-step: build the seqrepo-rest-service docker
 # See https://github.com/biocommons/seqrepo-rest-service
 
 # ----------------------------------------------------
-# --- 1. spin up seqrepo-rest-service
+# --- 1. spin up seqrepo-rest-service, if needed
 # ----------------------------------------------------
-docker run \
-  --name seqrepo-rest-service \
-  --user=`id -u`:`id -g` \
-  --detach --rm -p 5000:5000 \
-  --network=host \
-  -v ${SEQ_REPO_DIR}:/usr/local/share/seqrepo \
-  biocommons/seqrepo-rest-service \
-  seqrepo-rest-service /usr/local/share/seqrepo
+
+[ `docker ps -f name="seqrepo-rest-service" | wc -l` -gt 1 ] \
+  || docker run \
+       --name seqrepo-rest-service \
+       --user=`id -u`:`id -g` \
+       --detach --rm -p 5000:5000 \
+       --network=host \
+       -v ${SEQ_REPO_DIR}:/usr/local/share/seqrepo \
+       biocommons/seqrepo-rest-service \
+       seqrepo-rest-service /usr/local/share/seqrepo
 
 
 # ...and wait for it to be available
