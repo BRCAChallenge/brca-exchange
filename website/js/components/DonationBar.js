6f404c0f2aa22baa1c36fe5a1c709fb515f4b72b
melissacline <melissa.s.cline@gmail.com>
2024-02-01 15:04:00 -0800

    Update DonationBar.js
    Removed trailing whitespace before period

--- website/js/components/DonationBar.js
+++ website/js/components/DonationBar.js
@@ -9,7 +9,7 @@ const DonationBar = React.createClass({
         return (
         	<div className="donation-bar">
     			<p>
-                We are pleased to introduce a new feature: persistent URLs for the variants in BRCA Exchange!  More information is available <a style={{textDecoration: 'underline'}} href="https://brcaexchange.org/blog/index.php/2024/02/01/new-feature-persistent-urls-for-brca-exchange-variants/">here</a> on the BRCA Exchange blog .
+                We are pleased to introduce a new feature: persistent URLs for the variants in BRCA Exchange!  More information is available <a style={{textDecoration: 'underline'}} href="https://brcaexchange.org/blog/index.php/2024/02/01/new-feature-persistent-urls-for-brca-exchange-variants/">here</a> on the BRCA Exchange blog.
 	        	</p>
         	</div>
         );
