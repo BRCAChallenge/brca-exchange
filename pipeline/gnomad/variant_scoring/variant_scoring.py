2e12ec80f142b29355dd9ef7b37c3812a4c3cffc
melissacline <cline@soe.ucsc.edu>
2024-02-20 17:58:33 -0800

    Removed dead code, fixed a bug that led to under-assignment of BS1_Supporting

--- pipeline/gnomad/variant_scoring/variant_scoring.py
+++ pipeline/gnomad/variant_scoring/variant_scoring.py
@@ -64,21 +64,6 @@ def read_flags(flag_data):
     return(flags)
     
 
-def var_obj_to_name(v):
-    return '-'.join(str(x) for x in [v.chr, v.pos, v.ref, v.alt])
-
-def var_name_to_obj(v):
-    a = v.split('-')
-    return variant_utils.VCFVariant(a[0], int(a[1]), a[2], a[3])
-
-
-csep = '-'
-def add_name_col(dfx: pd.DataFrame):
-    dfx['var_name'] = (dfx['contigName'].apply(str) + csep +
-                       (dfx['start']).apply(str) +
-                       csep + dfx['referenceAllele'] + csep + dfx['alternateAlleles'])
-    return dfx
-
 def estimate_coverage(start, end, chrom, df_cov):
     positions = list(range(start, end+1))
     coverage_this_chrom = df_cov.loc[df_cov["chrom"] == int(chrom)]
@@ -88,70 +73,7 @@ def estimate_coverage(start, end, chrom, df_cov):
     return(meanval, medianval)
 
 
-def add_filter_flag_col(df, cols):
-    df['vcf_filter_flag'] = df['filters'].apply(
-        lambda filt_el: len(set(filt_el.split(';')).intersection(cols)) > 0)
 
-
-def add_final_code_column(df):
-    success_codes = set(['BA1', 'BS1', 'pm2_supporting'])
-    dq_failure_codes = set(['fail_insufficient_read_depth', 'fail_vcf_filter_flag', 'need_review'])
-    CODE_MISSING = 'code_missing'
-    
-    def is_both_sources(dfg):
-        return len(dfg) > 1
-
-    def set_final_code(dfg):
-        if len(dfg) == 1:
-            return dfg['evidence_code'].iloc[0]
-        elif len(dfg) == 2:
-            # we have data from both v2 and v3
-            c1 = dfg['evidence_code'].iloc[0]
-            c2 = dfg['evidence_code'].iloc[1]
-
-            if (c1 in dq_failure_codes and c2 == CODE_MISSING) or (c2 in dq_failure_codes and c1 == CODE_MISSING):
-                return CODE_MISSING
-            
-            if c1 in dq_failure_codes and c2 in dq_failure_codes:
-                return "fail_both"
-            
-            if (c1 == 'pm2_supporting' and c2 == CODE_MISSING) or (c2 == 'pm2_supporting' and c1 == CODE_MISSING):
-                return CODE_MISSING
-
-            if c1 == CODE_MISSING and c2 == CODE_MISSING:
-                return CODE_MISSING
-            
-            if c2 in success_codes and c1 not in success_codes:
-                return c2
-            if c1 in success_codes and c2 not in success_codes:
-                return c1
-
-            assert c1 in success_codes and c2 in success_codes
-            
-            if c1 == c2:
-                return c1
-            else:
-                return "contradictory_results"
-
-        raise ValueError("some duplicate variants per source?")
-
-    v2_and_v3 = df.groupby('var_name').apply(is_both_sources)
-    per_variant_code = df.groupby('var_name').apply(set_final_code)
-
-    return df.merge(pd.DataFrame({'in_v2_and_v3' : v2_and_v3, 'final_code': per_variant_code}).reset_index(), how='left')
-
-
-def extract_variant_scoring_data(df_cov2, df_cov3, df_var2, df_var3, read_depth_thresh, resource_dir):
-    agg_v2 = process_v2(df_var2, df_cov2, read_depth_thresh, resource_dir)
-    agg_v3 = process_v3(df_var3, df_cov3, read_depth_thresh)
-
-    df_overall = pd.concat([agg_v2, agg_v3]).reset_index(drop=True)
-
-    # running the actual scoring algorithm and the combined data
-    df_overall['evidence_code'] = df_overall.apply(determine_evidence_code_per_variant, axis=1)
-    df_overall = add_final_code_column(df_overall)
-
-    return df_overall.sort_values('var_name')
 
 def initialize_output_file(input_file, output_filename):
     """
@@ -166,11 +88,13 @@ def initialize_output_file(input_file, output_filename):
     output_file.writeheader()
     return(output_file)
 
+
 def field_defined(field):
     """
     Return a binary value indicating whether or not this variant has the popmax FAF defined
     """
     return(field != "-")
+
 
 def analyze_one_dataset(faf95_popmax_str, faf95_population, allele_count, is_snv,
                         mean_read_depth, median_read_depth, vcf_filter_flag, debug=True):
@@ -289,9 +213,9 @@ def analyze_across_datasets(code_v2, faf_v2, faf_popmax_v2,
     elif code_v2 == BS1:
         return(BS1, BS1_MSG % (faf_v2, faf_popmax_v2))
     elif code_v3 == BS1_SUPPORTING:
-        return(BS1, BS1_SUPPORTING_MSG % (faf_v3, faf_popmax_v3))
+        return(BS1_SUPPORTING, BS1_SUPPORTING_MSG % (faf_v3, faf_popmax_v3))
     elif code_v2 == BS1_SUPPORTING:
-        return(BS1, BS1_SUPPORTING_MSG % (faf_v2, faf_popmax_v2))
+        return(BS1_SUPPORTING, BS1_SUPPORTING_MSG % (faf_v2, faf_popmax_v2))
     #
     # If we get here, there is a hole in the logic
     return(FAIL_NEEDS_REVIEW, FAIL_NEEDS_SOFTWARE_REVIEW_MSG)
@@ -323,7 +247,6 @@ def analyze_variant(variant, coverage_v2, coverage_v3, flags_v2, flags_v3, debug
         (mean_read_depth, median_read_depth) = estimate_coverage(int(variant["pyhgvs_Hg37_Start"]),
                                                                  int(variant["pyhgvs_Hg37_End"]),
                                                                  int(variant["Chr"]),coverage_v2)
-        print("mean read depth", mean_read_depth, "median read depth", median_read_depth)
         if debug:
             print("gnomAD V2 variant", variant["Variant_id_GnomAD"], "popmax", variant["faf95_popmax_exome_GnomAD"],
                   "allele count", variant["Allele_count_exome_GnomAD"], "mean read depth", mean_read_depth,
@@ -339,7 +262,6 @@ def analyze_variant(variant, coverage_v2, coverage_v3, flags_v2, flags_v3, debug
         (mean_read_depth, median_read_depth) = estimate_coverage(int(variant["Hg38_Start"]),
                                                                  int(variant["Hg38_End"]),
                                                                  int(variant["Chr"]),coverage_v3)
-        print("mean read depth", mean_read_depth, "median read depth", median_read_depth)
         if debug:
             print("gnomAD V3 variant", variant["Variant_id_GnomADv3"], "popmax", variant["faf95_popmax_genome_GnomADv3"],
                   "allele count", variant["Allele_count_genome_GnomADv3"], "mean read depth", mean_read_depth,
