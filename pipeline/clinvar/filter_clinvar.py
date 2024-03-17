38698bc1baa8e5cef09cfa9fb3b4d3a683597206
melissacline <melissa.s.cline@gmail.com>
2024-03-17 10:27:32 -0700

    Updated the ClinVar XML path.  Updated the ClinVar parser to adapt to changes in the XML structure.  Credit to Perplexity AI!  Updated the SeqRepo path to use the latest SeqRepo archive rather than a literal version

--- pipeline/clinvar/filter_clinvar.py
+++ pipeline/clinvar/filter_clinvar.py
@@ -1,57 +1,39 @@
-'''
-Parses original ClinVar XML file and filters out ClinVarSets of interest
-'''
+#!/usr/bin/env python
 
+import argparse
 import gzip
-import itertools
+import xml.etree.ElementTree as ET
 
-import click
-from lxml import etree
-from clinvar import clinvar_common
+def filter_xml_for_gene_symbol(input_fp, output_fp, target_symbol_values,
+                               chunk_size=1024):
+    for event, elem in ET.iterparse(input_fp, events=('start', 'end')):
+        if event == 'start' and elem.tag == "ClnVarVariationRelease":
+            output_FP.write(ET.tostring(elem, encoding='UTF-8'))
+        if event == 'end' and elem.tag == 'VariationArchive':
+            gene_element = elem.find('.//Gene')
+            if gene_element is not None:
+                if "Symbol" in gene_element.attrib:
+                    if gene_element.attrib["Symbol"] in target_symbol_values:
+                        print("Victory!!", gene_element.attrib["Symbol"])
+                        output_fp.write(ET.tostring(elem, encoding='UTF-8'))
+            elem.clear()
+    output_fp.write("</ClinVarVariationRelease>".encode('UTF-8'))
 
-def open_maybe_gzip(fname):
-    if fname.endswith('gz'):
-        return gzip.GzipFile(fname)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', "--input")
+    parser.add_argument('-o', "--output")
+    parser.add_argument('-g', "--gene", action='append')  
+    args = parser.parse_args()
+    if args.input.endswith('gz'):
+        input_fp = gzip.open(args.input)
     else:
-        return open(fname, 'r')
-
-
-def filter_xml(fin, fout, symbols):
-    fout = open(fout, 'wb')
-
-    xpath_filter = clinvar_common.build_xpath_filter_for_cv_assertions(symbols)
-
-    # copying header
-    with open_maybe_gzip(fin) as f:
-        header_lines = itertools.takewhile(lambda s: "ClinVarSet" not in s.decode('utf-8'), f)
-        fout.writelines(header_lines)
-
-    # filtering ClinVarSet's we are interested in
-    with open_maybe_gzip(fin) as f:
-        for _, el in etree.iterparse(f, tag='ClinVarSet'):
-            if len(el.xpath(xpath_filter)) >= 1:
-                fout.write(etree.tostring(el, pretty_print=True, encoding='UTF-8'))
-
-            # some cleanup to save a significant amount of memory
-            # (inspired by https://www.ibm.com/developerworks/xml/library/x-hiperfparse/)
-            el.clear()
-            # Also eliminate now-empty references from the root node to elem
-            for ancestor in el.xpath('ancestor-or-self::*'):
-                while ancestor.getprevious() is not None:
-                    del ancestor.getparent()[0]
-
-    # writing "footer"
-    fout.write("</ReleaseSet>".encode('UTF-8'))
-
-    fout.close()
-
-
-@click.command()
-@click.argument('input', type=click.Path(exists=True))
-@click.argument('output', type=click.Path())
-@click.option('--gene', type=str, required=True, multiple=True)
-def main(input, output, gene):
-    filter_xml(input, output, list(set(gene)))
+        input_fp = open(args.input, 'r')
+    output_fp = open(args.output, 'wb')
+    filter_xml_for_gene_symbol(input_fp, output_fp, args.gene)
 
 if __name__ == "__main__":
     main()
+
+
