325c5935deaca22ffa0a39c8f59920d81dc758be
melissacline <melissa.s.cline@gmail.com>
2024-08-07 17:17:21 -0700

    Fixed a bug that led to the first line of the first VariationArchive record being included erroneously

--- pipeline/clinvar/filter_clinvar.py
+++ pipeline/clinvar/filter_clinvar.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
-"""
 
-filter_clinvar.py: Filter the ClinVar VCV Release XML file, and build a subset
-XML file containing the VariationArchive records for the genes of interest.
-
-This file filters the XML by reading the XML file as text, assembling text 
-objects of the VariationArchive records, and then parsing those records as XML
-to see if they specify the target gene symbols.  It's done this way because:
-- reading the whole file into one big ElementTree XML object doesn't work 
-  because it's too large and we run out of memory
-- reading the file iteratively, with ElementTree iterparse hasn't worked, 
-  because the ClinVar archive contains some badly-formatted XML records.
-  By not parsing the XML as XML until we know that we're looking at an object
-  that we want, we can wager that the broken XML records are for other genes.
-"""
 import argparse
 import gzip
 import io
@@ -37,12 +23,12 @@ def filter_xml_for_gene_symbol(input_fp, output_fp, target_symbol_values,
     in_variation_archive = False
     in_header = True
     for line in input_fp:
-        if in_header:
-            output_fp.write(line)
         if '<VariationArchive' in line:
             in_variation_archive = True
             in_header = False
             variation_archive = io.StringIO()
+        if in_header:
+            output_fp.write(line)
         if in_variation_archive:
             variation_archive.write(line)
         if '</VariationArchive>' in line:
@@ -54,17 +40,6 @@ def filter_xml_for_gene_symbol(input_fp, output_fp, target_symbol_values,
                 output_fp.write(xml_string)
 
 
-def old(input_fp, output_fp, target_symbol_values,  chunk_size=1024):
-    for event, elem in ET.iterparse(input_fp, events=('start', 'end')):
-        if event == 'start' and elem.tag == "ClnVarVariationRelease":
-            output_FP.write(ET.tostring(elem, encoding='UTF-8'))
-        if event == 'end' and elem.tag == 'VariationArchive':
-            gene_element = elem.find('.//Gene')
-            if gene_element is not None:
-                if "Symbol" in gene_element.attrib:
-                    if gene_element.attrib["Symbol"] in target_symbol_values:
-                        output_fp.write(ET.tostring(elem, encoding='UTF-8'))
-            elem.clear()
 
 
 def main():
@@ -80,7 +55,7 @@ def main():
         input_fp = open(args.input, 'r')
     output_fp = open(args.output, 'w')
     filter_xml_for_gene_symbol(input_fp, output_fp, args.gene)
-    output_fp.write("</ClinVarVariationRelease>".encode('UTF-8'))
+    output_fp.write("</ClinVarVariationRelease>")
 
 if __name__ == "__main__":
     main()
