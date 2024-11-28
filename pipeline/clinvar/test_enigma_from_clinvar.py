8698f05d39da280c44141829b1b040c050a237f6
melissacline <melissa.s.cline@gmail.com>
2024-11-28 14:22:59 -0800

    Updated the ENIGMA ClinVar test and test data

--- pipeline/clinvar/test_enigma_from_clinvar.py
+++ pipeline/clinvar/test_enigma_from_clinvar.py
@@ -15,9 +15,6 @@ from . import hgvs_utils
     ['p.K862E:AAG>GAG', False],
     ['Q1313X', False]
 ])
-def test_is_bic_designation(s, expected):
-    assert enigma_from_clinvar._is_bic_designation(s) == expected
-
 
 def test_parse_record(hgvs_wrapper):
     hgvs_util = hgvs_utils.HGVSWrapper(hgvs_wrapper.hgvs_dp)
@@ -29,25 +26,21 @@ def test_parse_record(hgvs_wrapper):
     res = enigma_from_clinvar.parse_record(cvs_el, hgvs_util, ['BRCA1', 'BRCA2'])
 
     expected = [{
-        'Abbrev_AA_change': 'I562Mfs*12',
         'Allele_origin': 'Germline',
         'Assertion_method': 'ENIGMA BRCA1/2 Classification Criteria (2017-06-29)',
-        'Assertion_method_citation': 'https://submit.ncbi.nlm.nih.gov/ft/byid/ncs3jnil/enigma_rules_2017-06-29.pdf',
+        'Assertion_method_citation': 'https://submit.ncbi.nlm.nih.gov/ft/byid/vuhooppz/enigma_rules_2017-06-29-v2.5.1.pdf',
         'BIC_Nomenclature': None,
-        'ClinVarAccession': 'SCV000783130.1',
+        'ClinVarAccession': 'SCV000783591.1',
         'Clinical_significance': 'Pathogenic',
         'Clinical_significance_citations': '',
         'Collection_method': 'Curation',
         'Comment_on_clinical_significance': 'Variant allele predicted to encode a truncated non-functional protein.',
-        'Condition_ID_type': 'OMIM',
-        'Condition_ID_value': 'BREAST-OVARIAN CANCER, FAMILIAL, SUSCEPTIBILITY TO, 1; BROVCA1 (604370)',
-        'Condition_category': 'Disease',
-        'Date_last_evaluated': '2017-12-15',
+        'Date_last_evaluated': '2018-07-13',
         'Gene_symbol': 'BRCA1',
-        'Genomic_Coordinate': 'chr17:43093845:A>ACTTTC',
-        'HGVS_cDNA': 'c.1685_1686insGAAAG',
-        'HGVS_protein': 'p.(Ile562MetfsTer12)',
-        'Reference_sequence': 'NM_007294.3',
+        'Genomic_Coordinate': 'chr17:43094316:TTGATTCAGACTCCCCATCATGTGAGTCATCAGAACCTAACA>T',
+        'HGVS_cDNA': 'c.1175_1215del',
+        'HGVS_protein': 'p.Leu392fs',
+        'Reference_sequence': 'NM_007294.4',
         'URL': None,
     }]
 
