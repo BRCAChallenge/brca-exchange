ab022ae20423ac94112d4a3a6c1b1778063bc4d2
melissacline <cline@soe.ucsc.edu>
2025-12-17 19:34:54 -0800

    Files changed to move to django5

--- website/django/data/urls.py
+++ website/django/data/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.index, name="index"),
-    url(r'^releases', views.releases, name="releases"),
-    url(r'^variant/$', views.variant, name="variant"),
-    url(r'^vrid/$', views.vrid, name="vr_id"),
-    url(r'^variant/(?P<variant_id>.+)/reports$', views.variant_reports, name="variant_reports"),
-    url(r'^variantcounts', views.variant_counts, name="variant_counts"),
-    url(r'^variantpapers/$',views.variant_papers, name="variant_papers"),
-    url(r'^variantreps/$', views.variantreps, name="variant_reps"),
-    url(r'^sitemap.txt$', views.sitemap, name="sitemap"),
-    url(r'^suggestions/$', views.autocomplete)
+    re_path(r'^$', views.index, name="index"),
+    re_path(r'^releases', views.releases, name="releases"),
+    re_path(r'^variant/$', views.variant, name="variant"),
+    re_path(r'^vrid/$', views.vrid, name="vr_id"),
+    re_path(r'^variant/(?P<variant_id>.+)/reports$', views.variant_reports, name="variant_reports"),
+    re_path(r'^variantcounts', views.variant_counts, name="variant_counts"),
+    re_path(r'^variantpapers/$',views.variant_papers, name="variant_papers"),
+    re_path(r'^variantreps/$', views.variantreps, name="variant_reps"),
+    re_path(r'^sitemap.txt$', views.sitemap, name="sitemap"),
+    re_path(r'^suggestions/$', views.autocomplete)
 
 ]
--- website/django/users/urls.py
+++ website/django/users/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
-from rest_framework_jwt.views import obtain_jwt_token
+from django.urls import re_path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from . import views
 
 urlpatterns = [
-    url(r'^register/$', views.register, name="register"),
-    url(r'^users/$', views.users, name="users"),
-    url(r'^user_locations/$', views.user_locations, name="user_locations"),
-    url(r'^token-auth/$', obtain_jwt_token, name="token_auth"),
-    url(r'^get/$', views.retrieve, name="retrieve"),
-    url(r'^update/$', views.update, name="update"),
-    url(r'^password_reset/$', views.password_reset, name="password_reset"),
-    url(r'^confirm/(?P<activation_key>\w+)/$', views.confirm, name="confirm"),
-    url(r'^resend-activation/$', views.resend_activation, name="resend_activation"),
-    url(r'^update_password/(?P<password_reset_token>\w+)/$', views.update_password, name="update_password"),
-    url(r'^check_password_token/(?P<password_reset_token>\w+)/$', views.check_password_token, name="check_password_token"),
+    re_path(r'^register/$', views.register, name="register"),
+    re_path(r'^users/$', views.users, name="users"),
+    re_path(r'^user_locations/$', views.user_locations, name="user_locations"),
+    re_path(r'^token-auth/$', TokenObtainPairView.as_view(), name="token_auth"),
+    re_path(r'^get/$', views.retrieve, name="retrieve"),
+    re_path(r'^update/$', views.update, name="update"),
+    re_path(r'^password_reset/$', views.password_reset, name="password_reset"),
+    re_path(r'^confirm/(?P<activation_key>\w+)/$', views.confirm, name="confirm"),
+    re_path(r'^resend-activation/$', views.resend_activation, name="resend_activation"),
+    re_path(r'^update_password/(?P<password_reset_token>\w+)/$', views.update_password, name="update_password"),
+    re_path(r'^check_password_token/(?P<password_reset_token>\w+)/$', views.check_password_token, name="check_password_token"),
 ]
