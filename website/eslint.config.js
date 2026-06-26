bab8a9bae21a51ecc71cccc2ac8d09938ad94a2a
GeoffLyle <geofflyle@gmail.com>
2026-06-26 20:56:06 +0000

    Fix ESLint for CircleCI

---
+++ website/eslint.config.js
@@ -0,0 +1,71 @@
+// eslint.config.js
+const js = require("@eslint/js");
+const react = require("eslint-plugin-react");
+const babelParser = require("@babel/eslint-parser");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  react.configs.flat.recommended,
+  {
+    files: ["js/**/*.js", "test/**/*.js"],
+    languageOptions: {
+      ...react.configs.flat.recommended.languageOptions,
+      parser: babelParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es6,
+        google: false,
+        grecaptcha: false,
+      },
+    },
+    rules: {
+      "camelcase": 2,
+      "dot-notation": [2, { "allowKeywords": false, "allowPattern": "^[a-zA-Z]+(_[a-zA-Z]+)+$" }],
+      "eol-last": 2,
+      "eqeqeq": [2, "allow-null"],
+      "curly": 2,
+      "key-spacing": 2,
+      "new-cap": [2, { "capIsNew": false }],
+      "no-dupe-keys": 2,
+      "no-extra-bind": 2,
+      "no-extra-boolean-cast": 2,
+      "no-trailing-spaces": 2,
+      "no-use-before-define": 2,
+      "space-infix-ops": 2,
+      "space-before-blocks": 2,
+      "yoda": 0,
+      "no-extra-parens": [2, "functions"],
+      "comma-spacing": 2,
+      "no-undef": 2,
+      "no-redeclare": [2, { "builtinGlobals": true }],
+      "no-underscore-dangle": 0,
+      "no-mixed-spaces-and-tabs": 0,
+      "no-multi-spaces": 0,
+      "strict": [2, "global"],
+      "no-console": 0,
+      "quotes": 0,
+      "no-shadow": 0,
+      "no-unused-vars": [2, { "vars": "local" }],
+      "react/jsx-no-undef": 1,
+      "react/jsx-uses-react": 1,
+      "react/jsx-uses-vars": 1,
+      "react/no-did-mount-set-state": 1,
+      "react/no-did-update-set-state": 1,
+      "react/no-unknown-property": 1,
+      "react/react-in-jsx-scope": 1,
+      "react/self-closing-comp": 1,
+      "react/jsx-wrap-multilines": 1,
+      "semi": [2, "always"],
+    },
+  },
+];
