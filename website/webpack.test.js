0b25c31fbcda427915371a5ba3d093a6ef50ac49
GeoffLyle <geofflyle@gmail.com>
2026-07-14 00:03:35 +0000

    Add build commands for CircleCI

--- website/webpack.test.js
+++ website/webpack.test.js
@@ -1,7 +1,8 @@
 var webpack = require('webpack');
+var path = require('path');
 var config = require('./webpack.config');
 
 config.output.filename = "testBundle.js";
-config.output.path = "./build";
+config.output.path = path.resolve(__dirname, 'build');
 config.entry = './test/all.js';
 module.exports = config;
